fbf8a40353b939ec454a9faa396309f07ae85d2b
Ales Teska <ales.teska@teskalabs.com>
2019-12-01 14:19:39 +0100

    Add a public export of the Tenant module.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import "babel-polyfill";
+
 export {
 	default as Application,
 	ApplicationContext
 } from './containers/Application';
+
+
+export {
+	default as TenantModule
+} from './modules/tenant/module';
