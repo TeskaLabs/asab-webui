8f22d03c618583ce13ac7f9227884d1ff6afdc74
Ales Teska <ales.teska@teskalabs.com>
2020-03-21 21:02:37 +0100

    Export of the tenant module.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import "babel-polyfill";
 
 export {default as Application} from './containers/Application';
 export {getBaseName} from './basename';
+
+export {
+	default as TenantModule
+} from './modules/tenant/module';
