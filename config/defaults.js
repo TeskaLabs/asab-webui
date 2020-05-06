dfc9a36d33449fb3682ba5e3f22217a490e47eb9
Ales Teska <ales.teska@teskalabs.com>
2020-05-07 00:55:19 +0200

    Removal of the obsolete concept.

--- config/defaults.js
+++ config/defaults.js
@@ -5,7 +5,6 @@ const CWD = process.cwd();
 module.exports = {
 	app: {
 		"publicUrl": "",	// Overriden by --public-url [PUBLIC_URL] command line argument
-		"title": "TeskaLabs ASAB Web UI Kit",
 	},
 
 	webpack: {
