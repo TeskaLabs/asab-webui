defa2c6b212720796f6ec39f2c0ae3cb33c22b85
F.P <pesekf@email.cz>
2020-08-18 15:55:59 +0200

    Add default property configuration for axios url, add alerts

--- src/config/ConfigService.js
+++ src/config/ConfigService.js
@@ -9,31 +9,33 @@ export default class ConfigService extends Service {
 	constructor(app, serviceName="ConfigService") {
 		super(app, serviceName);
 		app.ReduxService.addReducer("config", ConfigReducer);
-
 	}
 
 	initialize() {
 		this.App.addSplashScreenRequestor(this);
-		// TODO url must be in default property
-		this.Axios = this.App.axiosCreate('configuration/');
-
-		this.Axios.get('config/').then(response => {
-				// TODO check if response 200, OK etc, write error message
-				this.inject(response.data);
+		// TODO test it on empty configuration / no configuration
+		this.Axios = this.App.axiosCreate(this.App.Config._config.configUrl);
+		this.Axios.get().then(response => {
+				// TODO implement check on status codes
+				if (response.statusText === 'OK') {
+					this.inject(response.data);
+				} else {
+					this.App.addAlert("danger", "Something went wrong. Config file could not have been loaded.");
+				}
+				console.log(response)
 			})
 			.catch(error => {
-				// TODO error messsage
 				console.log(error); 
-				this.App.addAlert("danger", error.toString());
+				this.App.addAlert("danger", "File not found. The path might be corrupted.");
 			})
 			.then(() => this.App.removeSplashScreenRequestor(this));
 	}
 
+	// Inject configuration from site to the App Store
 	inject(config) {
 		this.App.Store.dispatch({
 			type: CHANGE_CONFIG,
 			config: config
 		})
 	}
-
 }
