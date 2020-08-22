17d3337cd6ce982793f1f00b1cb5c6ea741af74b
Ales Teska <ales.teska@teskalabs.com>
2020-08-22 13:21:26 +0200

    Evolution in date/time display.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "babel-polyfill";
 
-export {default as Application} from './containers/Application';
-export {getBaseName} from './basename';
+export { default as Application } from './containers/Application';
+export { getBaseName } from './basename';
+
+export { DateTime } from './containers/DateTime';
 
 import "./style.scss";
