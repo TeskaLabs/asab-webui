e57c8847692c4fe5d5eaacfc420a8e15df30a4b4
F.P <pesekf@email.cz>
2020-09-21 10:36:59 +0200

    Fix indentation

--- demo/src/modules/home/index.js
+++ demo/src/modules/home/index.js
@@ -3,13 +3,13 @@ import Module from 'asab-webui/abc/Module';
 
 
 export default class HomeModule extends Module {
-    constructor(app, name){
-        super(app, "HomeModule");
-        app.Router.addRoute({ path: '/', exact: true, name: 'Home', component: HomeContainer });
-        app.Navigation.addItem({
+	constructor(app, name){
+		super(app, "HomeModule");
+		app.Router.addRoute({ path: '/', exact: true, name: 'Home', component: HomeContainer });
+		app.Navigation.addItem({
 			name: 'Home',
 			url: '/',
 			icon: 'cil-home',
-        });
-    }
+		});
+	}
 }
