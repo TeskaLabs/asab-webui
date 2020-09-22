979cd4fb4d9c6a3042f141e2c9aba7115dc67f7c
F.P <pesekf@email.cz>
2020-09-22 18:05:43 +0200

    Update with new config settings, adjust rendering

--- src/modules/tools/ToolsDashboard.js
+++ src/modules/tools/ToolsDashboard.js
@@ -9,7 +9,7 @@ export default class ToolsDashboard extends Component {
 
 	/* Example of use
 
-	{"TOOLS":{"Kibana":{"url":127.0.0.1.,"path":"tools/kibana.svg"}}}
+	{"tools":[{"name":"Kibana","url":127.0.0.1.,"path":"tools/kibana.svg"}}}
 
 	*/
 
@@ -26,7 +26,7 @@ export default class ToolsDashboard extends Component {
 			config = config;
 			this.App.addAlert("warning", 'Unable to load the data from site.');
 		} else {
-			config = this.Config.get('TOOLS');
+			config = this.Config.get('tools');
 		}
 		return (
 			config !== undefined ?
@@ -37,10 +37,19 @@ export default class ToolsDashboard extends Component {
 						return(
 							<React.Fragment key={key}>
 								<Col sm={3} className="text-center">
-									<a href={config[key].url} target="_blank">
-										<img src={config[key].path} style={{width:200+"px", height:200+"px"}}></img>
-									</a>
-									<h1>{key}</h1>
+									<Row className="justify-content-center">
+										<a href={config[key].url} target="_blank">
+											<img
+												src={config[key].path}
+												style={{width:200+"px", height:200+"px"}}
+												title={config[key].url}
+												>
+											</img>
+										</a>
+									</Row>
+									<Row className="justify-content-center pt-2">
+										<h1>{config[key].name}</h1>
+									</Row>
 								</Col>
 							</React.Fragment>
 						)
