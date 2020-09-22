c515e948639ef8936978c4a968bd92901c177ab9
F.P <pesekf@email.cz>
2020-09-22 14:22:06 +0200

    Create module Tools

---
+++ src/modules/tools/ToolsDashboard.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import {
+	Container, Col, Row
+} from "reactstrap";
+
+
+export default class ToolsDashboard extends Component {
+
+	/* Example of use
+
+	{"TOOLS":{"Kibana":{"url":127.0.0.1.,"path":"tools/kibana.svg"}}}
+
+	*/
+
+	constructor(props) {
+		super(props);
+		this.App = props.app;
+		this.Config = this.App.Config;
+	}
+
+
+	render() {
+		let config = undefined;
+		if (this.Config === undefined) {
+			config = undefined;
+			this.App.addAlert("warning", 'Unable to load the data from site.');
+		} else {
+			config = this.Config.get('TOOLS');
+		}
+		return (
+			config !== undefined ?
+			<React.Fragment>
+				<Container fluid className="animated fadeIn">
+					<Row>
+					{Object.keys(config).map((key, idx) => {
+						return(
+							<React.Fragment key={key}>
+								<Col sm={3} className="text-center">
+									<a href={config[key].url} target="_blank">
+										<img src={config[key].path} style={{width:200+"px", height:200+"px"}}></img>
+									</a>
+									<h1>{key}</h1>
+								</Col>
+							</React.Fragment>
+						)
+					})}
+					</Row>
+				</Container>
+			</React.Fragment>
+			: null
+		);
+	}
+};
---
+++ src/modules/tools/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Module from 'asab-webui/abc/Module';
+import {
+	Container, Col,
+	Link
+} from "reactstrap";
+import ToolsDashboard from "./ToolsDashboard";
+
+export default class ToolsModule extends Module {
+	constructor(app, name) {
+		super(app, "ToolsModule");
+		this.App = app;
+		this.Config = app.config;
+		this.Navigation = app.Navigation;
+		this.Router = app.Router;
+
+
+		this.Router.addRoute({
+			path: "/tools",
+			exact: true,
+			name: "Tools",
+			component: ToolsDashboard,
+		});
+
+		this.Navigation.addItem({
+			name: "Tools",
+			url: "/tools",
+			icon: 'cil-puzzle',
+		});
+	}
+}
