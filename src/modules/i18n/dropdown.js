ea751d7e5cdb2b8e2fbbb6f86a32858ffc9073b0
Ales Teska <ales.teska@teskalabs.com>
2020-10-17 13:11:40 +0200

    The internatiolization now allows to switch the language.

---
+++ src/modules/i18n/dropdown.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+
+import {
+	UncontrolledDropdown,
+	Dropdown,
+	DropdownItem,
+	DropdownMenu, 
+	DropdownToggle,
+	Nav, NavItem, NavLink
+} from 'reactstrap';
+
+import {
+	AppAsideToggler,
+	AppHeaderDropdown,
+	AppNavbarBrand,
+	AppSidebarToggler
+} from '@coreui/react';
+
+
+function LanguageDropdown(props) {
+
+	const { t, i18n } = useTranslation();
+
+	const logout = (language) => {
+		i18n.changeLanguage(language).then(() => {
+			window.location.reload(false);
+		});
+	}
+
+	return (
+		<UncontrolledDropdown direction="down" className="pr-3">
+			<DropdownToggle nav caret>
+				<LanguageFlag language={i18n.language}/>
+			</DropdownToggle>
+			<DropdownMenu right>
+				<DropdownItem header>Language</DropdownItem>
+
+				{i18n.options.supportedLngs.map((language, i) => (
+					<DropdownItem key={language} title={language} onClick={() => {logout(language)}}>
+						<LanguageFlag language={language}/>
+						{t('i18n|language|'+language)}
+					</DropdownItem>
+				))}
+
+			</DropdownMenu>
+		</UncontrolledDropdown>
+	);
+}
+
+export default LanguageDropdown;
+
+let LanguageToFlag = {
+	cs: 'cif-cz',
+	en: 'cif-gb',
+	dev: 'cil-bug',
+	cimode: 'cil-bug',
+}
+
+function LanguageFlag(props) {
+	let lcls = LanguageToFlag[props.language]
+	return (<i className={lcls +' pr-2'} style={{height: '1em'}}></i>)
+}
--- src/modules/i18n/index.js
+++ src/modules/i18n/index.js
@@ -1,5 +1,6 @@
 import Module from 'asab-webui/abc/Module';
 import I18nService from './service.js';
+import LanguageDropdown from './dropdown.js';
 
 export default class I18nModule extends Module {
 
@@ -7,6 +8,11 @@ export default class I18nModule extends Module {
 		super(app, "I18nModule");
 		this.App = app;
 		this.I18nService = new I18nService(app, "I18nModule");
+	}
+
+	initialize() {
+		const headerService = this.App.locateService("HeaderService");
+		headerService.addComponent(LanguageDropdown);
 	}
 
 }
