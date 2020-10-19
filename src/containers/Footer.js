bbf988a2406eb4219ef5a97fa0e1510961f0cf03
F.P <pesekf@email.cz>
2020-10-19 15:16:21 +0200

    Update with site configuration

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import { connect } from 'react-redux';
 import { AppFooter } from '@coreui/react';
+import axios from 'axios';
+
+
+/*
+	Example of use branding configuration from site:
+
+	Site config file:
+
+	{"branding": "path/to/the/configuration/file.json"}
+
+
+	JSON file with configuration:
+
+	{
+		"footer_image": {
+			"src": "path/outside/of/project/header-full.svg",
+			"alt": "Created by FooBar",
+			"href": "http://foobar.fb"
+		}
+	}
+
+*/
 
 
 export function Footer(props) {
 
 	let FooterService = props.app.locateService("FooterService");
+
+	// Get header branding from site
+	let branding_url = props.app.Config.get("branding");
+	// Set state for footer images
+	const [siteFooterImageSrc, setSiteFooterImageSrc] = useState(props.footer_image.src);
+	const [siteFooterImageAlt, setSiteFooterImageAlt] = useState(props.footer_image.alt);
+	const [siteFooterImageHref, setSiteFooterImageHref] = useState(props.footer_image.href);
+	// Check on undefined configuration
+	if (branding_url !== undefined) {
+		axios.get(branding_url).then(response => {
+			if (response.data.footer_image !== undefined) {
+				if (response.data.footer_image.src !== undefined) {
+					setSiteFooterImageSrc(response.data.footer_image.src);
+				}
+				if (response.data.footer_image.alt !== undefined) {
+					setSiteFooterImageAlt(response.data.footer_image.alt);
+				}
+				if (response.data.footer_image.href !== undefined) {
+					setSiteFooterImageHref(response.data.footer_image.href);
+				}
+			}
+		})
+		.catch(error => {console.log(error)})
+	}
 
 	return (
 		<AppFooter>
@@ -15,12 +61,12 @@ export function Footer(props) {
 			))}
 
 			<span className="ml-auto">
-				<a target="_blank" href={props.footer_image.href}>
+				<a target="_blank" href={siteFooterImageHref}>
 					<img
 						height={16}
 						width={120}
-						alt={props.footer_image.alt}
-						src={props.footer_image.src}
+						alt={siteFooterImageAlt}
+						src={siteFooterImageSrc}
 					/>
 				</a>
 			</span>
