18a82e3b90ffed6e4143ca6b2b315416aaaa7cc8
F.P <pesekf@email.cz>
2021-01-12 17:53:01 +0100

    Implement axios to get overall and parameter dataset

--- src/modules/asab-config/ConfigDashboard.js
+++ src/modules/asab-config/ConfigDashboard.js
@@ -6,8 +6,60 @@ import {
 	Button
 } from "reactstrap";
 
-export default function ConfigDashboard() {
-	const [ data, setData ] = useState();
+export default function ConfigDashboard(props) {
+
+	let App = props.app;
+	let Config = App.Config;
+	let url = undefined;
+
+	const [ data, setData ] = useState({});
+	const [ paramData, setParamData ] = useState({});
+
+	// Retrieve the ASAB_CONFIG_URL from config file
+	if (!Config?.get('ASAB_CONFIG_URL')) {
+		url = url;
+		App.addAlert("warning", 'Unable to load the data. `ASAB_CONFIG_URL` is not defined.')
+	} else {
+		url = Config.get('ASAB_CONFIG_URL');
+	}
+
+	useEffect(() => {
+		getData();
+	},[]);
+
+	// Obtain the overall dataset
+	const getData = () => {
+		if (url) {
+			let Axios = App.axiosCreate(url);
+			// TODO: replace the get parameter with `Axios.get("/config")` to obtain all the main components from zookeeper 
+			Axios.get("/config/new_type"
+				).then(response => {
+					// console.log(response.data)
+					setData(response.data);
+				})
+				.catch(error => {
+					console.log(error); // log the error to the browser's console
+					App.addAlert("warning", t('Unable to get data: ', { error: error.toString() }));
+			});
+		}
+	}
+
+	// Obtain the data for specific config_type
+	const getParameterData = (configType) => {
+		if (data) {
+			let Axios = App.axiosCreate(url);
+			Axios.get("/config/" + configType.toString()
+				).then(response => {
+					setParamData(response.data);
+				})
+				.catch(error => {
+					console.log(error); // log the error to the browser's console
+					App.addAlert("warning", t('Unable to get data: ', { error: error.toString() }));
+			});
+		} else {
+			App.addAlert("warning", 'No data has been retrieved.')
+		}
+	}
 
 	return (
 		<React.Fragment>
