b5a253c1930b8d9edccb11f3ab720f1ad45ac520
F.P <pesekf@email.cz>
2021-01-13 15:18:13 +0100

    Implement authorization on available tenants

--- src/modules/tenant/service.js
+++ src/modules/tenant/service.js
@@ -16,60 +16,58 @@ export default class TenantService extends Service {
 		const search = window.location.search;
 		const params = new URLSearchParams(search);
 		var tenant_id = params.get('tenant');
-		// var payload = {};
 		
 		// Fetch tenants
 		this.API.get_tenants()
 		.then(response => {
-			let payload = response.data; 
+			let tenants = response.data;
 
-			if (payload.length == 0) {
+			if (tenants.length == 0) {
 				this.App.addAlert("danger", "No tenants avaiable, got an empty tenant list.", 40000);
 				return;
 			}
 
-			// If tenant has not been provided in access URL, pick a first tenant from a list 
-			if (tenant_id == null) {
-				tenant_id = payload[0]._id;
-				// ... and refresh (reload) the whole web app
-				window.location.replace('?tenant='+tenant_id+'#/');
-				return;
-			}
+			// Authorize user
+			let payload = [];
+			Object.values(tenants).map((tenant, idx) => {
+				this.API.get_user_access(tenant._id, this.OAuthToken['access_token']).then(response => {
+					if (response.data.result == 'OK'){
+						payload.push(tenant)
+					}
+					if (idx + 1 == tenants.length) {
+						if (payload.length == 0) {
+							this.App.addAlert("danger", "You are not authorized to use this application.", 40000);
+							return;
+						}
 
-			// Find the current tenant in the list and extract its
-			let x = payload.filter((item) => { return item._id == tenant_id } );
-			if (x.length < 1) {
-				this.App.addAlert("danger", "Invalid tenant :-(", 40000);
-				return;
-			}
+						// If tenant has not been provided in access URL, pick a first tenant from a list
+						if (tenant_id == null || tenant_id !== payload[0]._id) {
+							tenant_id = payload[0]._id;
+							// ... and refresh (reload) the whole web app
+							window.location.replace('?tenant='+tenant_id+'#/');
+							return;
+						}
 
-			this.App.Store.dispatch({
-				type: types.TENANTS_CHANGED,
-				payload,
-				current: x[0],
-			});
+						// Find the current tenant in the list and extract its
+						let x = payload.filter((item) => { return item._id == tenant_id } );
+						if (x.length < 1) {
+							this.App.addAlert("danger", "Invalid tenant :-(", 40000);
+							return;
+						}
 
-			console.log(x, 'x tenants')
+						this.App.Store.dispatch({
+							type: types.TENANTS_CHANGED,
+							payload,
+							current: x[0],
+						});
 
-			// this.API.get_user_access(tenant, this.OAuthToken['access_token'])
-			if (this.OAuthToken['access_token'] == null) {
-				console.log('je nulll')
-			} else {
-				console.log(payload, 'payload')
-				console.log(this.OAuthToken['access_token'], 'access token')
-				Object.values(payload).map((tenant, idx) => {
-					console.log(tenant._id)
-					this.API.get_user_access(tenant._id, this.OAuthToken['access_token']).then(response => {
-						console.log(tenant._id , 'tenantid')
-						console.log(response.data)
-					}).catch((error) => {
-						console.log(error);
-						this.App.addAlert("danger", "Failed to load tenants.", 40000);
-					})
+						this.App.removeSplashScreenRequestor(this);
+					}
+				}).catch((error) => {
+					console.log(error);
+					this.App.addAlert("danger", "Failed to load tenants.", 40000);
 				})
-			}
-
-			this.App.removeSplashScreenRequestor(this);
+			})
 		})
 		.catch((error) => {
 			console.log(error);
