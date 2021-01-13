a4c71ec11f310174f30f37a0c54569f89ceae2cc
F.P <pesekf@email.cz>
2021-01-13 12:55:57 +0100

    Implement RBAC endpoint to get user access based on tenant

--- src/modules/tenant/api.js
+++ src/modules/tenant/api.js
@@ -1,9 +1,16 @@
 export default class Api {
-    constructor(axios) {
-        this.Axios = axios;
-    }
+	constructor(axios) {
+		this.Axios = axios;
+	}
 
-    get_tenants() {
-        return this.Axios.get('/tenant')
-    }
+	get_tenants() {
+		return this.Axios.get('/tenant')
+	}
+
+	get_user_access(tenant, access_token) {
+		return this.Axios.get(
+			"/rbac/" + tenant + "/tenant:access",
+			{ headers: { 'Authorization': 'Bearer ' + access_token }}
+			)
+	}
 }
