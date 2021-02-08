e395fd15edf4d92b9b2cc923da3d0ef5c1b1403a
F.P <pesekf@email.cz>
2021-02-08 13:33:23 +0100

    Update header component of auth module with SERVICE auth path, provide backward compatibility with USER_AUTH_URL

--- src/modules/auth/header.js
+++ src/modules/auth/header.js
@@ -11,7 +11,12 @@ import {
 function HeaderComponent(props) {
 
 	const Config = props.app.Config;
-	let user_auth_url = Config.get('USER_AUTH_URL') ? Config.get('USER_AUTH_URL') : Config.get('BASE_URL');
+	let user_auth_url = Config.get('SERVICES')?.auth ? Config.get('SERVICES')?.auth : Config.get('BASE_URL');
+
+	// Provide backward compatibility with USER_AUTH_URL
+	if (Config.get('USER_AUTH_URL')) {
+		user_auth_url = Config.get('USER_AUTH_URL');
+	}
 
 	const logout = () => {
 		props.AuthModule.logout()
