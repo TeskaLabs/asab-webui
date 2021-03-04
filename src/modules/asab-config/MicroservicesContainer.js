4fce5c5d11b28e4a4f5fa31372450843ef0af2c2
Dzmitry Tsitkou <titkov49@gmail.com>
2021-03-04 13:46:19 +0100

    update table with new response

--- src/modules/asab-config/MicroservicesContainer.js
+++ src/modules/asab-config/MicroservicesContainer.js
@@ -6,24 +6,48 @@ import {
     ListGroup, ListGroupItem
 } from 'reactstrap';
 
+import { DataTable } from 'asab-webui';
+
 export default (props) => {
-    const [list, setList] = useState([]);
+    const [list, setList] = useState(null);
+    const [page, setPage] = useState(1);
+    const headers = [{name: 'Name', key: 'name'}, {name: 'IP', key: 'ip'}];
+    const limit = 10;
 
     useEffect(() => {
+        const parseDataIntoArr = (data) => {
+            return Object.keys(data).map(name => {
+                const item = data[name];
+                return {
+                    ...item,
+                    name: name,
+                    ip: item.web[0][0]
+                };
+            });
+        };
+
         const ASABConfigAPI = props.app.axiosCreate('asab_config');
-        ASABConfigAPI.get('/svcs').then(res => setList(console.log(res.data)));
+
+        ASABConfigAPI.get('/svcs').
+            then(res => parseDataIntoArr(res.data)).
+            then(lst => setList(lst));
     }, []);
 
     return (
         <Container>
-            <Card>
-                <CardHeader>Microservices List</CardHeader>
-                <CardBody>
-                    <ListGroup>
-                        {list.map((item, idx) => <ListGroupItem key={idx}><b>{item}</b></ListGroupItem>)}
-                    </ListGroup>
-                </CardBody>
-            </Card>
+            {list && 
+                <DataTable 
+                    headers={headers}
+                    data={list.slice((page-1)*limit, limit*page)}
+                    page={page}
+                    setPage={setPage}
+                    count={list.length}
+                    limit={limit}
+                    title={{
+                        text: "Microservices List"
+                    }}
+                />
+            }
         </Container>
     )
 }
