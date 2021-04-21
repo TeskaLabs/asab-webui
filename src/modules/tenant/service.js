e853f2752ec7b93398f4a0336efc6795f52585c8
F.P <pesekf@email.cz>
2021-04-21 11:42:53 +0200

    Refactor tenant module for case that tenants are not passed in userinfo

--- src/modules/tenant/service.js
+++ src/modules/tenant/service.js
@@ -16,9 +16,8 @@ export default class TenantService extends Service {
 		const search = window.location.search;
 		const params = new URLSearchParams(search);
 		var tenant_id = params.get('tenant');
-
 		// If tenant has not been provided in access URL, pick a first tenant from a list
-		if (tenant_id == null) {
+		if (tenant_id == null && tenants_list != null) {
 			tenant_id = tenants_list[0];
 			// ... and refresh (reload) the whole web app
 			window.location.replace('?tenant='+tenant_id+'#/');
@@ -26,17 +25,22 @@ export default class TenantService extends Service {
 		}
 
 		// Find the current tenant in the list and extract it
-		let current_tenant = tenants_list.filter((item) => { return item == tenant_id } );
-		if (current_tenant.length < 1) {
-			this.App.addAlert("danger", "Invalid tenant :-(", 40000);
-			return;
+		let current_tenant;
+		if (tenants_list) {
+			let filtered_tenant = tenants_list.filter((item) => { return item == tenant_id });
+			if (filtered_tenant.length < 1) {
+				this.App.addAlert("danger", "Invalid tenant :-(", 40000);
+				return;
+			}
+			current_tenant = filtered_tenant[0];
+		} else {
+			current_tenant = null;
 		}
-
 		// Dispatch tenants obtained from userinfo
 		this.App.Store.dispatch({
 			type: types.TENANTS_CHANGED,
 			tenants_list,
-			current: current_tenant[0],
+			current: current_tenant,
 		});
 
 	}
