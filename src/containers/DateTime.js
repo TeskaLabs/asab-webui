ddc61c2f69f71fa094bac64baa9cb619d103d5d5
dtsitkou <dzmitry.tsitkou@plus4u.net>
2021-05-17 14:46:13 +0200

    return datetime multiplier (1000)

--- src/containers/DateTime.js
+++ src/containers/DateTime.js
@@ -17,7 +17,7 @@ The input `value` is in UTC and one of:
 
 * Data() object (Javascript)
 * "2020-08-22T00:00:00+00:00" String with ISO format of the day
-* Number with Unix timestamp
+* Number with Unix timestamp (in seconds)
 
 The date&time will be converted to a local timezone of the browser.
 
@@ -35,7 +35,12 @@ export function DateTime(props) {
 		)
 	}
 
-	const m = moment(props.value);
+	let m = null;
+	if (isNaN(props.value)) {
+		m = moment(props.value);
+	} else {
+		m = moment(props.value * 1000.0);
+	}
 
 	return (
 		<span className="datetime" title={m.fromNow()}>
