4c7c291a8971aeb41ff5fd4cc854609326bd17b5
t-pyrope <kimmarina155@gmail.com>
2021-06-24 16:44:16 +0200

    change path from /auth/about to /about

--- src/modules/about/index.js
+++ src/modules/about/index.js
@@ -1,14 +1,14 @@
 import Module from '../../abc/Module';
 
 import AboutScreen from './AboutScreen';
-console.log("hello from AboutModule");
+
 export default class AboutModule extends Module {
 
 	constructor(app, name) {
 		super(app, "AboutModule");
 
 		app.Router.addRoute({
-			path: '/auth/about',
+			path: '/about',
 			exact: true,
 			name: 'About',
 			component: AboutScreen
