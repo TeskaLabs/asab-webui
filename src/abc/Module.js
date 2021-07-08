64800fbf7c844b2da4d9aa238106a2c94d8445af
Dzmitry Tsitkou <dzmitry.tsitkou@teskalabs.com>
2021-07-08 16:05:47 +0200

    convert spaves to tabs in Module file

--- src/abc/Module.js
+++ src/abc/Module.js
@@ -1,9 +1,9 @@
 export default class Module {
-    constructor(app, name) {
-    	this.App = app;
-        this.Name = name;
-    }
+	constructor(app, name) {
+		this.App = app;
+		this.Name = name;
+	}
 
-    initialize() {
-    }
+	initialize() {
+	}
 }
