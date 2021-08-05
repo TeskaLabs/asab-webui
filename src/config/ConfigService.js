45310dd4857d3d478088576dfbbf5ff8ddcca9af
Dzmitry Tsitkou <dzmitry.tsitkou@teskalabs.com>
2021-08-05 08:49:09 +0200

    add docs for new Config Service

--- src/config/ConfigService.js
+++ src/config/ConfigService.js
@@ -40,6 +40,8 @@ export default class ConfigService extends Service {
 
 
 	initialize() {
+		// dynamic_config_url is taken from content atribute of head element
+		// <meta name="x-config" content="https..." />
 		const dynamic_config_url = document.getElementsByName('x-config')[0]?.content;
 
 		// Check on undefined configuration
