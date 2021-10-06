c3810b0c1efe14850adeabe12bc030b658b51584
F.P <pesekf@email.cz>
2021-10-06 13:25:24 +0200

    Update tenant selector with Card, logout and styling

--- src/modules/tenant/selector/SelectTenantScreen.js
+++ src/modules/tenant/selector/SelectTenantScreen.js
@@ -1,32 +1,67 @@
 import React from 'react';
 import { connect } from 'react-redux';
+// import { useTranslation } from 'react-i18next';
 
 import {
-	Card, CardHeader, CardFooter, CardBody, CardTitle,
+	Card, CardHeader, CardFooter, CardBody, CardTitle, CardSubtitle,
 	Button, Input
 } from 'reactstrap';
 
+import "./select.css";
+
 function TenantSelectionScreen(props) {
+	// const { t } = useTranslation();
+	const SeaCatAuthAPI = props.app.axiosCreate('seacat_auth');
 
 	const selectTenant = (tn) => {
-		console.log(tn, "TENANNNT")
+		if (tn.value !== "99999") {
+			window.location.replace(`?tenant=${tn.value}#/`);
+			return;
+		}
+	}
+
+	const logout = async () => {
+		let response;
+		try {
+			response = await SeaCatAuthAPI.put('/public/logout');
+		}
+		catch (err) {
+			console.error("Failed to fetch userinfo", err);
+			props.app.addAlert("danger", "Silly as it sounds, the logout failed");
+		}
+		window.location.reload();
 	}
 
 	return (
-		props.tenants ?
-			<div className="text-center animated fadeIn" style={{marginTop: "10%"}}>
-				<Input
-					type="select"
-					name="selectTenant"
-					id="selectTenant"
-					onClick={(e) => {selectTenant(e.target)}}
-					defaultValue={props.current}
-				>
-					{props.tenants.length > 0 ? props.tenants.map((tenant, idx) => {return(
-						<option key={idx}>{tenant}</option>
-					)}) : null}
-				</Input>
-			</div>
+		props.app.Services.TenantService && props.app.Modules.filter(obj => obj.Name === "AuthModule").length > 0 && props.tenants && props.current == null ?
+				<Card className="tenant-selection-card">
+					<CardHeader>
+						<CardTitle>
+							Select valid tenant to enter the application
+							{/*{t("SelectTenantScreen|Select tenant")}*/}
+						</CardTitle>
+					</CardHeader>
+					<CardBody>
+						<Input
+							type="select"
+							name="selectTenant"
+							id="selectTenant"
+							onClick={(e) => {selectTenant(e.target)}}
+							defaultValue={props.current}
+						>
+							<option key="def" value="99999">Select tenant</option>
+							{props.tenants.length > 0 ? props.tenants.map((tenant, idx) => {return(
+								<option key={idx}>{tenant}</option>
+							)}) : null}
+						</Input>
+					</CardBody>
+					<CardFooter>
+						<Button color="primary" onClick={() => {logout()}}>
+							Logout
+							{/*{t("SelectTenantScreen|Logout")}*/}
+						</Button>
+					</CardFooter>
+				</Card>
 		: null
 	);
 }
