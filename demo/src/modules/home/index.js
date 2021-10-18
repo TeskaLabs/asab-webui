5999c12c6e54fa8f4756707404612f68ceea9253
Dzmitry Tsitkou <dzmitry.tsitkou@teskalabs.com>
2021-10-19 01:58:32 +0200

    create demo exmaple

--- demo/src/modules/home/index.js
+++ demo/src/modules/home/index.js
@@ -3,21 +3,28 @@ import Module from 'asab-webui/abc/Module';
 
 const HomeContainer = lazy(() => import('./containers/HomeContainer'));
 const TableContainer = lazy(() => import('./containers/TableContainer'));
+const ErrorHandlerExample = lazy(() => import('./containers/ErrorHandlerExample'));
 
 export default class HomeModule extends Module {
 	constructor(app, name){
 		super(app, "HomeModule");
-		app.Router.addRoute({ path: '/', exact: true, name: 'Home', component: HomeContainer });
-		app.Router.addRoute({ path: '/Table', exact: true, name: 'Table', component: TableContainer });
+		app.Router.addRoute({ path: '/home', exact: true, name: 'Home', component: HomeContainer });
+		app.Router.addRoute({ path: '/table', exact: true, name: 'Table', component: TableContainer });
+		app.Router.addRoute({ path: '/error-example', exact: true, name: 'Error Example', component: ErrorHandlerExample});
 		app.Navigation.addItem({
 			name: 'Home',
-			url: '/',
+			url: '/home',
 			icon: 'cil-home',
 		});
 		app.Navigation.addItem({
 			name: 'Table',
-			url: '/Table',
+			url: '/table',
 			icon: 'cil-chart',
+		});
+		app.Navigation.addItem({
+			name: 'Error Example',
+			url: '/error-example',
+			icon: 'cil-warning',
 		});
 	}
 }
