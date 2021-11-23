953eba66fc8ac4ea400bc6b930ee1f5577113a98
Dzmitry Tsitkou <dzmitry.tsitkou@teskalabs.com>
2021-11-23 15:21:47 +0100

    add suport for react-hook-form v6

--- src/containers/UncontrolledSwitch/index.js
+++ src/containers/UncontrolledSwitch/index.js
@@ -4,16 +4,16 @@ import { ControlledSwitch } from 'asab-webui';
 
 const UncontrolledSwitch = ({ 
 	defaultValue = false, disabled = false, 
-	title, register, setValue
+	title, register, setValue, name, id
 }) => {
 		const [isOn, setIsOn] = useState(defaultValue)
 
 		useEffect(() => {
-			setValue(register.name, defaultValue)
+			setValue(register.name || name, defaultValue)
 		},[])
 
 		useEffect(() => {
-			setValue(register.name, isOn, { shouldDirty: true })
+			setValue(register.name || name, isOn, { shouldDirty: true })
 		}, [isOn])
 
 		return (
@@ -24,7 +24,13 @@ const UncontrolledSwitch = ({
 					disabled={disabled}
 					toggle={() => setIsOn(prev => !prev)}
 				/>
-				<input type="checkbox" {...register} style={{display:"none"}}></input>
+				<input
+					type="checkbox"
+					name={name}
+					id={id}
+					{...register}
+					style={{ display:"none" }} 
+				/>
 			</>
 		)
 }
