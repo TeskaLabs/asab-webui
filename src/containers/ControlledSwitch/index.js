4ce8758ab7cf27f315ed14a6f57103a1a0f32c86
Dzmitry Tsitkou <dzmitry.tsitkou@teskalabs.com>
2021-11-23 01:15:36 +0100

    add disabled and title

--- src/containers/ControlledSwitch/index.js
+++ src/containers/ControlledSwitch/index.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './switch.scss';
 
-const Switch = ({ isOn, toggle }) => (
-	<div className="switch-container">
-		<div className={isOn ? "on" : "off"} onClick={toggle}>
-			<div className="circle"></div>
+const Switch = ({ isOn, toggle, disabled=false, title }) => {
+	const onClick = () => {
+		if (!disabled) toggle();
+	}
+	
+	return (
+		<div className={`switch-container${disabled ? "-disabled" : ""}`} title={title}>
+			<div className={isOn ? "on" : "off"} onClick={onClick}>
+				<div className="circle"></div>
+			</div>
 		</div>
-	</div>
-);
+	);
+}
 
 export default Switch;
