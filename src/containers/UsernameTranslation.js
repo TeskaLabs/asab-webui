02bdef22e27611bc06506edcd6b082edc967a782
petrKavulok <petr.kavulok@gmail.com>
2021-12-22 15:54:16 +0100

    naming updat to match SeaCat Auth convention

--- src/containers/UsernameTranslation.js
+++ src/containers/UsernameTranslation.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from "react-router-dom";
 
@@ -10,20 +10,21 @@ At the moment works ONLY with apiPath='seacat_auth'
 
 Usage:
 
-import { UsernameTranslation } from 'asab-webui';
+import { Credentials } from 'asab-webui';
 ...
-<UsernameTranslation app={props.app} id={['user1-id', 'user2-id', 'user3-id']} apiPath='seacat_auth' cleanupTime={1000 * 60 * 60 * 24 * 14}/>
+<Credentials app={props.app} id={['user1-id', 'user2-id', 'user3-id']} apiPath='seacat_auth' cleanupTime={1000 * 60 * 60 * 24 * 14}/>
 
 */
 
-export function UsernameTranslation(props) {
+export function Credentials(props) {
 
 	const { t } = useTranslation();
 
 	const apiPath = props.apiPath ?? 'seacat_auth';
 	let API = props.app.axiosCreate(apiPath);
 
-	const userIdsArray = Array.isArray(props.userId) ? props.userId : [props.userId] ;
+	// const userIdsArray = Array.isArray(props.userId) ? props.userId : [props.userId] ;
+	const userIdsArray = ['mongodb:ext:618be17866f6ee5de4ae5328', 'mongodb:ext:617fcfb940cfa90b4aefaf39', 'mongodb:ext:618bd4baa2548da08bdcb2e3'];
 
 	const cleanupTime = props.cleanupTime ?? 1000 * 60 * 60 * 24; // 24 hrs
 
@@ -34,36 +35,33 @@ export function UsernameTranslation(props) {
 		try {
 			let response = await API.put(`usernames`, userIdsArray);
 			if (response.data.result !== "OK") {
-				throw new Error(t("ASABUsernameTranslation|Something went wrong, failed to fetch assigned credentials"));
-			}
-			if (Object.keys(response.data.data).length === 0) {
-				throw new Error('..no data');
+				throw new Error(t("ASABCredentials|Something went wrong, failed to fetch assigned credentials"));
 			}
 			const usernamesToLS = setUsernamesToLS(response.data.data, userIdsArray, cleanupTime);
 			setUsernames(usernamesToLS);
 		} catch (e) {
-			props.app.addAlert("warning", t("ASABUsernameTranslation|Something went wrong, failed to fetch assigned credentials"));
+			props.app.addAlert("warning", t("ASABCredentials|Something went wrong, failed to fetch assigned credentials"));
 		}
 	}
 
 	// compares array of IDs with data in localstorage
-	const matchUserIds = (usernameIDs) => {
+	const matchUserIds = (credentials_id) => {
 		let currentTime = Date.now();
-		const usernamesInLS = getUsernamesFromLS('UsernameTranslations', cleanupTime);
+		const usernamesInLS = getUsernamesFromLS('Credentials', cleanupTime);
 		let usernamesToRender = [];
 
-		if (usernamesInLS.usernames == undefined || usernamesInLS.usernames.length === 0 || usernamesInLS.expiration <= currentTime) {
+		if (usernamesInLS.credentials == undefined || usernamesInLS.credentials.length === 0 || usernamesInLS.expiration <= currentTime) {
 			retrieveUserNames();
 			return;
 		}
 
-		for (let i = 0; i < usernameIDs.length; i++) {
-			const indexFromLS = usernamesInLS.usernames.findIndex((itemInLS) => itemInLS.id === usernameIDs[i]);
+		for (let i = 0; i < credentials_id.length; i++) {
+			const indexFromLS = usernamesInLS.credentials.findIndex((itemInLS) => itemInLS.id === credentials_id[i]);
 			if (indexFromLS === -1) {
 				retrieveUserNames();
 				return;
 			}
-		usernamesToRender.push({ username: usernamesInLS.usernames[indexFromLS].username, id: usernamesInLS.usernames[indexFromLS].id });
+		usernamesToRender.push({ username: usernamesInLS.credentials[indexFromLS].username, id: usernamesInLS.credentials[indexFromLS].id });
 		}
 		setUsernames(usernamesToRender);
 	}
@@ -86,12 +84,12 @@ export function UsernameTranslation(props) {
 					)
 				})
 				:
-				userIdsArray.map((userId) => {
+				userIdsArray.map((credentials_id) => {
 					return (
 						<div>
 							<i className="cil-user pr-1"></i>
-							<Link to={{ pathname: `/auth/credentials/${userId}` }}>
-								{userId}
+							<Link to={{ pathname: `/auth/credentials/${credentials_id}` }}>
+								{credentials_id}
 							</Link>
 						</div>
 					)
@@ -102,44 +100,44 @@ export function UsernameTranslation(props) {
 }
 
 // Get usernames from localstorage
-function getUsernamesFromLS(username, cleanupTime) {
+function getUsernamesFromLS(name, cleanupTime) {
 	let ls;
 	if (global.localStorage) {
 		try {
-			ls = JSON.parse(global.localStorage.getItem(username.toString()));
+			ls = JSON.parse(global.localStorage.getItem(name.toString()));
 		} catch (e) {
 			/*Ignore*/
 		}
 	}
-	return ls ? ls : { usernames: [], expiration: new Date().getTime() + cleanupTime };
+	return ls ? ls : { credentials: [], expiration: new Date().getTime() + cleanupTime };
 }
 
 function setUsernamesToLS(data, userIdsArray, cleanupTime) {
 	if (global.localStorage) {
-		let dataInLS = getUsernamesFromLS('UsernameTranslations', cleanupTime);
+		let dataInLS = getUsernamesFromLS('Credentials', cleanupTime);
 		let dataToLS = [];
-		userIdsArray.map((id) => {
+		userIdsArray.map((credential_id) => {
 			let item = {};
-			if (data[id]) {
+			if (data[credential_id]) {
 				item = {
-					id: id,
-					username: data[id],
+					id: credential_id,
+					username: data[credential_id],
 				};
 			}
-			if (!data[id]) {
+			if (!data[credential_id]) {
 				item = {
-					id: id,
-					username: 'N/A'
+					id: credential_id,
+					username: credential_id
 				}
 			}
-			const indexFromLS = dataInLS.usernames.findIndex((itemInLS) => itemInLS.id === item.id);
+			const indexFromLS = dataInLS.credentials.findIndex((itemInLS) => itemInLS.id === item.id);
 			if (indexFromLS === -1) {
-				dataInLS.usernames.push(item);
+				dataInLS.credentials.push(item);
 			}
 			dataToLS.push(item);
 		})
 		try {
-			global.localStorage.setItem('UsernameTranslations', JSON.stringify(dataInLS));
+			global.localStorage.setItem('Credentials', JSON.stringify(dataInLS));
 		}
 		catch (e) {
 			console.error(e);
