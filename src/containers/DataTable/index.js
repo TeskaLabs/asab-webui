03476b55d806689b03eeb2094851e95519e00b66
petrKavulok <79644454+petrKavulok@users.noreply.github.com>
2022-03-07 14:18:02 +0530

    Replacing spinner with ContentLoader in DataTable (#249)
    * replacing loader
    
    * obsolete code removal & CHANGELOG.md update

--- src/containers/DataTable/index.js
+++ src/containers/DataTable/index.js
@@ -17,7 +17,7 @@ import Table from './Table';
 import Pagination from '../Pagination';
 // import CustomDropdownButton from './CustomDropdownButton'; DON'T REMOVE YET. IT MAY BE USEFUL ON REFACTORING DATATABLE
 
-import { Spinner } from '../Spinner';
+import { CellContentLoader } from '../ContentLoader';
 import { ButtonWithAuthz } from '../../modules/auth/ButtonWithAuthz';
 
 export function DataTable ({
@@ -163,7 +163,7 @@ export function DataTable ({
 					<CardBody className="data-table-card-body">
 						{customCardBodyComponent}
 						{isLoading ? 
-							<div style={{ margin: "2rem auto" }}><Spinner /></div> :
+							<CellContentLoader cols={headers.length} rows={limit ?? 5} /> :
 							<Table
 								data={data.length > limit ? data.slice(0, limit) : data}
 								headers={headers}
