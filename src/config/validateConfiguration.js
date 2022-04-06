bd238f3b6540b7b67f2b1445ea0d1d280fa9f312
fpesek <frantisek.pesek@teskalabs.com>
2022-04-06 10:17:12 +0200

    Update function for config validation

--- src/config/validateConfiguration.js
+++ src/config/validateConfiguration.js
@@ -1,25 +1,73 @@
-// import React from 'react';
+/*
 
-// TODO: here I must implement check on :authorization >> to get the correct config section
+	Function for configuration validation with current tenant
 
-export const validateConfiguration = (props, config) => {
-	let currentTenant = props.app.Services.TenantService ? props.app.Services.TenantService.get_current_tenant() : undefined;
+	It will work only when Tenant module is loaded in the application
+	It require app props and configuration
+	Configuration has to be of type `object` (not array of objects)
+	It returns `false` if configuration does not match validation criteria on tenant
+	It returns `true` if configuration match the validation criteria on tenant
 
-	if (currentTenant && typeof config.tenants == "string") {
-		let tenantsSplit = config.tenants.toString().split(",");
-		let tenantsArray = [];
-		tenantsSplit.map(value => {
-			// Check if there is a whitespace in the first position of the string, and if so, erase that
-			if(value.substring(0,1) == " ") {
-				tenantsArray.push(value.substring(1));
-			} else {
-				tenantsArray.push(value);
-			}
-		})
-		if (tenantsArray.indexOf(currentTenant) == -1) {
-			return false;
+	Example of usage in the configuration:
+
+	```
+		"SectionName:authorization" {
+			"tenants": "tenant1, tenant2"
 		}
-		return true;
+	```
+
+	Example of usage within the code:
+
+	```
+	import { validateConfiguration } from 'asab-webui';
+
+	...
+
+
+		if (configuration && Object.keys(configuration).length > 0) {
+
+			...
+
+			// This part will manage if the configuration is suitable for further processing
+			if (validateConfiguration(props, configuration)) {
+				return;
+			}
+
+			// Processing configuration which passed the authorization criteria
+			...
+		}
+	```
+
+	The result of processing after validation should be that configuration, which has specified tenants
+	in the configuration and does match the criterion (match the configured tenants in the `:authorization`
+	section with current tenant of the user), will be visible only to the users, whos current tenant matches
+	the criteria.
+
+
+*/
+export const validateConfiguration = (props, config) => {
+	// Check if Tenant module is present within the application
+	if (props.app.Services.TenantService) {
+		let currentTenant = props.app.Services.TenantService.get_current_tenant();
+		// Get object with authorization setup (if defined in the configuration)
+		let authSection = Object.keys(config).filter(res => res.includes(`:authorization`));
+		// Check if current tenant exist and if authorization section is present within the configuration
+		if (currentTenant && config[authSection[0]]) {
+			let tenantsSplit = config[authSection[0]]?.tenants.toString().split(",");
+			let tenantsArray = [];
+			tenantsSplit.map(value => {
+				// Check if there is a whitespace in the first position of the string, and if so, erase that
+				if(value.substring(0,1) == " ") {
+					tenantsArray.push(value.substring(1));
+				} else {
+					tenantsArray.push(value);
+				}
+			})
+			if (tenantsArray.indexOf(currentTenant) == -1) {
+				return true;
+			}
+		}
+		return false;
 	}
-	return true;
+	return false;
 }
