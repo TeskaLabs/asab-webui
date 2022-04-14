df3b9abce5644db87185a283730d47dc74248bdf
petrKavulok <petr.kavulok@gmail.com>
2022-04-14 10:36:01 +0200

    docs update

--- src/components/DateTime/index.js
+++ src/components/DateTime/index.js
@@ -5,33 +5,6 @@ import { format, formatDistanceToNow, parseISO } from 'date-fns';
 import useDateFNSLocale from './useDateFNSLocale';
 import timeToString from './timeToString';
 
-/*
-Diplays a date & time in a local timezone.
-It also display the relative time in the hover/title.
-
-Usage:
-
-import { DateTime } from 'asab-webui';
-...
-
-<DateTime value="2020-08-22T00:00:00+00:00"/>
-
-
-The input `value` is in UTC and one of:
-
-* Data() object (Javascript)
-* "2020-08-22T00:00:00+00:00" String with ISO format of the day
-* Number with Unix timestamp in seconds (max value is 9999999999)
-* Number with timestamp in milliseconds (min value is 10000000000)
-
-The date&time will be converted to a local timezone of the browser.
-
-
-You may specify the format of the date&time, for details see https://momentjs.com.
-The default format is `lll` -> `Aug 22, 2020 1:13 PM`
-
-*/
-
 export function DateTime(props) {
 	const locale = useDateFNSLocale();
 
