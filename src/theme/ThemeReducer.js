9b3033b14d8de64337e2526b8cd9a9a3a0fb5926
Ales Teska <ales.teska@teskalabs.com>
2022-05-10 23:49:22 +0200

    Refactoring.

--- src/theme/ThemeReducer.js
+++ src/theme/ThemeReducer.js
@@ -1,6 +1,6 @@
 import { CHANGE_THEME } from "./actions";
 
-const themeInitState = "light";
+const themeInitState = null;
 
 export default function ThemeReducer(state = themeInitState, action) {
 	switch (action.type) {
@@ -8,18 +8,6 @@ export default function ThemeReducer(state = themeInitState, action) {
 		case CHANGE_THEME: 
 			const html = document.querySelector('html');
 			html.dataset.theme = action.theme;
-			const metaColorScheme = document.querySelector('meta[name="color-scheme"]');
-			if (metaColorScheme == null) {
-				// Create <meta ... /> for the color scheme
-				var meta = document.createElement('meta');
-				meta.name = "color-scheme";
-				meta.content = action.theme;
-				document.getElementsByTagName('head')[0].appendChild(meta);
-
-			} else {
-				// Set the color scheme
-				metaColorScheme.setAttribute("content", action.theme);
-			}
 			return action.theme;
 		
 		default:
