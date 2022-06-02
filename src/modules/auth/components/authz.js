1dc2b1ab7b492b69b403985a048805ae39d4b9c0
fpesek <frantisek.pesek@teskalabs.com>
2022-06-02 18:04:11 +0200

    Add defensive approach

--- src/modules/auth/components/authz.js
+++ src/modules/auth/components/authz.js
@@ -3,9 +3,9 @@ import { useTranslation } from 'react-i18next';
 export const authz = (childProps) => {
 	const { t, i18n } = useTranslation();
 
-	let disabled = childProps.resources ? childProps.resources.indexOf(childProps.resource) === -1 : true;
+	let disabled = childProps.resources && childProps.resource ? childProps.resources.indexOf(childProps.resource) === -1 : true;
 	// If user is superuser, then button is enabled
-	if (childProps.resources.indexOf('authz:superuser') !== -1) {
+	if (childProps.resources && childProps.resources.indexOf('authz:superuser') !== -1) {
 		disabled = false;
 	}
 	// If defined, hide the disabled button
