b2494910f864ed53a85dd4c8e2169867ae417ee7
akonshina <arina.konshina@teskalabs.com>
2022-07-11 13:16:30 +0200

    add function onClick, change action

--- src/containers/Sidebar/NavbarBrand.js
+++ src/containers/Sidebar/NavbarBrand.js
@@ -1,42 +1,63 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 
 import { Link } from 'react-router-dom';
+import { SET_SMALL_SIDEBAR } from "../../actions";
 
-const NavbarBrand = ({ title, brand_image, isSidebarCollapsed }) => {
+const NavbarBrand = ({ title, brand_image, isSmallSidebarOpen }) => {
 	const href = brand_image.href ?? "/";
+	const dispatch = useDispatch();
+
+	const onCollapse = () => {
+		dispatch({
+			type: SET_SMALL_SIDEBAR,
+			isSmallSidebarOpen: !isSmallSidebarOpen
+		});
+	}
 
 	if (href.includes("http")) {
 		return (
-			<div className="sidebar-brand-image">
-				<a
-					href={href}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img
-						src={isSidebarCollapsed ? brand_image.minimized : brand_image.full}
-						alt={title}
-						width="50"
-						height="50"
-						className="minimized-image"
-					/>
-				</a>
-			</div>
+			<>
+				<div className="header-sidebar-toggler" onClick={onCollapse}>
+					<i className="cil-menu"></i>
+				</div>
+				<div className="sidebar-brand-image">
+					<a
+						href={href}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img
+							// src={isSidebarCollapsed ? brand_image.minimized : brand_image.full}
+							src={brand_image.full}
+							alt={title}
+							width="50"
+							height="50"
+							className="minimized-image"
+						/>
+					</a>
+				</div>
+			</>
 		);
 	}
 	return (
-		<div className={`sidebar-brand-image`}>
-			<Link to={href}>
-				<img
-					src={isSidebarCollapsed ? brand_image.minimized : brand_image.full}
-					alt={title}
-					width="150"
-					height="50"
-					className="minimized-image"
-				/>
-			</Link>
-		</div>
+		<>
+			<div className="header-sidebar-toggler" onClick={onCollapse}>
+				<i className="cil-menu"></i>
+			</div>
+			<div className={`sidebar-brand-image`}>
+				<Link to={href}>
+					<img
+						// src={isSidebarCollapsed ? brand_image.minimized : brand_image.full}
+						src={brand_image.full}
+						alt={title}
+						width="150"
+						height="50"
+						className="minimized-image"
+					/>
+				</Link>
+			</div>
+		</>
 	)
 };
 
