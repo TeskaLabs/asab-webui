e47f76229c067ecf0b4a15862ef441815b225c7a
fpesek <frantisek.pesek@teskalabs.com>
2022-07-27 16:10:53 +0200

    Update locales

--- src/modules/knowledge/containers/KnowledgeBaseContainer.js
+++ src/modules/knowledge/containers/KnowledgeBaseContainer.js
@@ -86,7 +86,7 @@ export const KnowledgeBase = ({ props, apiPath, entryPath }) => {
 				setTreeData(tree);
 		} catch(e) {
 			console.error(e);
-			props.app.addAlert("warning", t("KnowledgeBaseContainer|Something went wrong, failed to fetch Knowledge base folder content"));
+			props.app.addAlert("warning", t("KnowledgeBase|Something went wrong, failed to fetch Knowledge base folder content"));
 		}
 	}
 
@@ -111,8 +111,8 @@ export const KnowledgeBase = ({ props, apiPath, entryPath }) => {
 			setText(response.data);
 		} catch(e) {
 			console.error("Failed to fetch article: ", e);
-			props.app.addAlert("warning", t("KnowledgeBaseContainer|Something went wrong, failed to get the article"));
-			setText(t("KnowledgeBaseContainer|There is no article of that name and path"));
+			props.app.addAlert("warning", t("KnowledgeBase|Something went wrong, failed to get the article"));
+			setText(t("KnowledgeBase|There is no article of that name and path"));
 		}
 		// scroll to the beginning of div
 		readmeDiv?.scrollTo({ top: 0 })
