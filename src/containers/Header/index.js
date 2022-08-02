9f1cf078b11989bebf8a1ecbdb241d4f1bbc7a93
akonshina <arina.konshina@teskalabs.com>
2022-08-02 17:02:12 +0200

    add condition for bradcrumbs

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -43,7 +43,7 @@ export function Header(props) {
 							</Link>
 						</div>
 					}
-					<Breadcrumbs app={props.app}/>
+					{windowDimensions.width > 768 &&<Breadcrumbs app={props.app}/>}
 					<Nav className="ml-auto header-props" navbar>
 						<ThemeButton />
 						<HelpButton />
@@ -131,6 +131,7 @@ export function Header(props) {
 				</>
 			}
 			</div>
+			{windowDimensions.width < 768 &&<Breadcrumbs app={props.app}/>}
 		</header>
 	);
 }
