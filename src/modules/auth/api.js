bdf1085ecba5facd2298cd6434a9614ecbe8798e
fpesek <frantisek.pesek@teskalabs.com>
2022-08-09 13:24:42 +0200

    Remove userinfo API path containing tenant

--- src/modules/auth/api.js
+++ src/modules/auth/api.js
@@ -55,14 +55,6 @@ export class SeaCatAuthApi {
 	userinfo(access_token) {
 		let userinfoPath = '/userinfo';
 		let headers = {};
-		// Obtain active tenant from URL params (if available)
-		const search = window.location.search;
-		const params = new URLSearchParams(search);
-		let active_tenant = params.get('tenant');
-		// Check if active tenant is present and eventually change the userinfo path
-		if (active_tenant) {
-			userinfoPath = '/userinfo?tenant=' + active_tenant.toString();
-		}
 		// Add access bearer token to the Authorization headers
 		if (access_token != null) {
 			headers.Authorization = 'Bearer ' + access_token;
