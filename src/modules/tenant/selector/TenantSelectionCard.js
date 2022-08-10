c3306f83ae9b265e36b8557b75d0c2283bf10410
fpesek <frantisek.pesek@teskalabs.com>
2022-08-10 09:39:11 +0200

    Update obtaining resources from store

--- src/modules/tenant/selector/TenantSelectionCard.js
+++ src/modules/tenant/selector/TenantSelectionCard.js
@@ -12,8 +12,7 @@ import "./select.scss";
 function TenantSelectionCard(props) {
 	const { t } = useTranslation();
 	const SeaCatAuthAPI = props.app.axiosCreate('seacat_auth');
-	let resources = props.userinfo?.resources[props.current];
-	let superuser = resources ? resources.indexOf('authz:superuser') !== -1 : false;
+	let superuser = props.resources ? props.resources.indexOf('authz:superuser') !== -1 : false;
 
 	const selectTenant = (tn) => {
 		if (tn.value !== "99999") {
@@ -73,6 +72,7 @@ function TenantSelectionCard(props) {
 function mapStateToProps(state) {
 	return {
 		userinfo: state.auth?.userinfo,
+		resources: state.auth?.resources,
 		tenants: state.tenant?.tenants,
 		current: state.tenant?.current
 	}
