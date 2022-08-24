e57ef8a0e0eee61280e8021ceb251fb613b9ceb0
fpesek <frantisek.pesek@teskalabs.com>
2022-08-24 15:28:13 +0200

    Add small description for locationReplace function

--- src/components/locationReplace.js
+++ src/components/locationReplace.js
@@ -1,3 +1,25 @@
+/*
+
+Use only within async functions as per example below.
+locationReplace function help to avoid unintended async actions
+before window.location.replace has finished.
+
+
+```
+import { locationReplace } from 'asab-webui';
+
+const myFunc = async () => {
+	...
+
+	const url = "url/to/replace";
+	await locationReplace(url);
+	...
+}
+
+```
+
+*/
+
 export const locationReplace = async (url) => {
 	window.location.replace(url.toString());
 	await new Promise(r => setTimeout(r, 3600 * 1000)); // Basically wait forever, this the app is going to be reloaded
