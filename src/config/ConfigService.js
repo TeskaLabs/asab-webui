92192b2b667a2871cedc15f893da8f9d63dbb413
fpesek <frantisek.pesek@teskalabs.com>
2022-09-02 17:06:56 +0200

    WIP test sub_filter injection from nginx

--- src/config/ConfigService.js
+++ src/config/ConfigService.js
@@ -39,48 +39,35 @@ export default class ConfigService extends Service {
 	}
 
 
-	async initialize() {
-		// // dynamic_config_url is taken from content atribute of meta element
-		// // <meta name="x-config" content="https..." />
-		// const dynamic_config_url = document.getElementsByName('x-config')[0]?.content;
-		// console.log(dynamic_config_url, "DYNAMIC CONFIG URL")
-		// console.log(document.getElementsByName('x-config'), "DOC BY ELEMENTS NAME")
+	initialize() {
+		const headerLogoFull = document.getElementsByName('header-logo-full')[0]?.content;
+		const headerLogoMini = document.getElementsByName('header-logo-minimized')[0]?.content;
+		const customCSS = document.getElementsByName('custom-css-file')[0]?.content;
+		const title = document.getElementsByName('title')[0]?.content;
 
-		// const request = new XMLHttpRequest();
-		// request.open("GET", document.location, true);
-		// request.send();
-		// request.onreadystatechange = () => {
-		// 	console.log(request.readyState, "READY STATE")
-		// 	if (request.readyState === XMLHttpRequest.DONE) {
-		// 		// Get the raw header string
-		// 		const headers = request.getAllResponseHeaders();
-		// 		console.log(headers, "HEADERS")
-		// 		if ((headers) && (typeof headers == 'string') && (headers.indexOf("x-config") != -1)) {
-		// 			// Convert the header string into an array
-		// 			// of individual headers
-		// 			const arr = headers.trim().split(/[\r\n]+/);
-		// 			console.log(arr, "ARRAY")
-		// 			// Create a map of header names to values
-		// 			const headerMap = {};
-		// 			arr.forEach((line) => {
-		// 				const parts = line.split(': ');
-		// 				const header = parts.shift();
-		// 				const value = parts.join(': ');
-		// 				headerMap[header] = value;
-		// 			});
-		// 			const xConfigPath = headerMap["x-config"];
-		// 			if (this.App.Store != null) {
-		// 				this.Config.dispatch(this.App.Store, xConfigPath);
-		// 				// this.App.Store.dispatch({ type: SET_DYNAMIC_CONFIG_PATH, dynamic_config_path: xConfigPath });
-		// 				console.log('DISPATCHED')
-		// 			}
-		// 			console.log(headerMap, headerMap["x-config"], "TAK ZKOUSIM")
-		// 		}
-		// 		console.log("NEPROSLO NEVADI")
-		// 	}
-		// }
-		// this.App.addSplashScreenRequestor(this);
+		let dynamicConfig = {};
+		let brandImage = {};
+		if (headerLogoFull != undefined) {
+			brandImage["full"] = headerLogoFull;
+			dynamicConfig["brand_image"] = brandImage;
+		}
+		if (headerLogoMini != undefined) {
+			brandImage["minimized"] = headerLogoMini;
+			dynamicConfig["brand_image"] = brandImage;
+		}
+		if (customCSS != undefined) {
+			// TODO
+		}
+		if (title != undefined) {
+			dynamicConfig["title"] = title;
+		}
 
+		if (Object.keys(dynamicConfig).length > 0) {
+			this.Config._dynamic_config = dynamicConfig;
+			if (this.App.Store !== undefined) {
+				this.Config.dispatch(this.App.Store);
+			}
+		}
 
 		/*
 			Example of nginx setup
@@ -88,70 +75,142 @@ export default class ConfigService extends Service {
 			location /<location> {
 				root /<path>/<to>/<build>;
 				index index.html;
-				add_header x-config "/<path>/<to>/<external_folder>/<config>.json"; #header to dynamic config
-			}
-
-			# location to dynamic config
-			location /<location>/<path>/<to>/<external_folder> {
-				alias /<path>/<to>/<external_folder>;
-				try_files $uri /<config>.json;
-				index <config>.json;
-				autoindex on;
-				autoindex_exact_size off;
-				autoindex_format json;
+				sub_filter '<meta name="header-logo-full">' '<head><meta name="header-logo-full" content="/<location>/<path>/<to>/<logo-full>.svg">';
+				sub_filter '<meta name="header-logo-minimized">' '<meta name="header-logo-minimized" content="/<location>/<path>/<to>/<logo-minimized>.svg">';
+				sub_filter '<meta name="title">' '<meta name="title" content="Custom app title">';
+				sub_filter '<meta name="custom-css-file">' '<meta name="custom-css-file" content="/<location>/<path>/<to>/<custom-file>.css">';
+				sub_filter_once on;
 			}
 
 
-			Example of config setup
+			Example of index.html setup
 
-			{
-				"brand_image": {
-					"full": "<path>/<to>/<external_folder>/logo-full.svg"
-				}
-			}
+			<head>
 
+			....
+
+			<!-- Dynamic config start -->
+			<meta name="header-logo-full">
+			<meta name="header-logo-minimized">
+			<meta name="custom-css-file">
+			<meta name="title">
+			<!-- Dynamic config end -->
+
+			...
+
+			</head>
 
 		*/
-		const dynamic_url = async () => {
-			const getCfgPath = await fetch(document.location).then(res => {
-				let xConfigPath = res.headers.get('x-config');
-				// if (this.App.Store != null) {
-				// 	// this.Config.dispatch(this.App.Store, xConfigPath);
-				// 	this.App.Store.dispatch({ type: SET_DYNAMIC_CONFIG_PATH, dynamic_config_path: xConfigPath });
-				// 	console.log(xConfigPath, 'DISPATCHED')
-				// }
-				// alert(res.headers.get('x-config'))
-				return xConfigPath;
-			}).catch((e) => {
-				console.error(e);
-				return undefined;
-			})
-			return getCfgPath;
-		}
-		// .then(() => this.App.removeSplashScreenRequestor(this));
-		const dynamic_config_url = await dynamic_url();
-		// Check on undefined configuration
-		if (dynamic_config_url !== undefined) {
-			this.App.addSplashScreenRequestor(this);
-			let axios = Axios.create({ baseURL: window.location.protocol + '//' + window.location.host });
-			axios.get(dynamic_config_url).then(response => {
-				// Check on status and content-type
-				if ((response.status === 200) && (response.headers["content-type"] !== undefined && response.headers["content-type"].includes("application/json"))) {
-					this.Config._dynamic_config = response.data;
-					if (this.App.Store !== undefined) {
-						this.Config.dispatch(this.App.Store);
-					}
-				} else {
-					this.App.addAlert("danger", "ASABConfigService|Incorrect/invalid config file downloaded", 5, true);
-				}
-			})
-				.catch(error => {
-					console.log(error);
-					this.App.addAlert("danger", "ASABConfigService|Error when downloading a config file. The path might be corrupted", 5, true);
-				})
-				.then(() => this.App.removeSplashScreenRequestor(this));
-		}
+
 	}
+
+	// async initialize() {
+	// 	// // dynamic_config_url is taken from content atribute of meta element
+	// 	// // <meta name="x-config" content="https..." />
+	// 	// const dynamic_config_url = document.getElementsByName('x-config')[0]?.content;
+	// 	// console.log(dynamic_config_url, "DYNAMIC CONFIG URL")
+	// 	// console.log(document.getElementsByName('x-config'), "DOC BY ELEMENTS NAME")
+
+	// 	// const request = new XMLHttpRequest();
+	// 	// request.open("GET", document.location, true);
+	// 	// request.send();
+	// 	// request.onreadystatechange = () => {
+	// 	// 	console.log(request.readyState, "READY STATE")
+	// 	// 	if (request.readyState === XMLHttpRequest.DONE) {
+	// 	// 		// Get the raw header string
+	// 	// 		const headers = request.getAllResponseHeaders();
+	// 	// 		console.log(headers, "HEADERS")
+	// 	// 		if ((headers) && (typeof headers == 'string') && (headers.indexOf("x-config") != -1)) {
+	// 	// 			// Convert the header string into an array
+	// 	// 			// of individual headers
+	// 	// 			const arr = headers.trim().split(/[\r\n]+/);
+	// 	// 			console.log(arr, "ARRAY")
+	// 	// 			// Create a map of header names to values
+	// 	// 			const headerMap = {};
+	// 	// 			arr.forEach((line) => {
+	// 	// 				const parts = line.split(': ');
+	// 	// 				const header = parts.shift();
+	// 	// 				const value = parts.join(': ');
+	// 	// 				headerMap[header] = value;
+	// 	// 			});
+	// 	// 			const xConfigPath = headerMap["x-config"];
+	// 	// 			if (this.App.Store != null) {
+	// 	// 				this.Config.dispatch(this.App.Store, xConfigPath);
+	// 	// 				// this.App.Store.dispatch({ type: SET_DYNAMIC_CONFIG_PATH, dynamic_config_path: xConfigPath });
+	// 	// 				console.log('DISPATCHED')
+	// 	// 			}
+	// 	// 			console.log(headerMap, headerMap["x-config"], "TAK ZKOUSIM")
+	// 	// 		}
+	// 	// 		console.log("NEPROSLO NEVADI")
+	// 	// 	}
+	// 	// }
+	// 	// this.App.addSplashScreenRequestor(this);
+
+
+
+	// 		Example of nginx setup
+
+	// 		location /<location> {
+	// 			root /<path>/<to>/<build>;
+	// 			index index.html;
+	// 			add_header x-config "/<path>/<to>/<external_folder>/<config>.json"; #header to dynamic config
+	// 		}
+
+	// 		# location to dynamic config
+	// 		location /<location>/<path>/<to>/<external_folder> {
+	// 			alias /<path>/<to>/<external_folder>;
+	// 			try_files $uri /<config>.json;
+	// 			index <config>.json;
+	// 			autoindex on;
+	// 			autoindex_exact_size off;
+	// 			autoindex_format json;
+	// 		}
+
+
+	// 		Example of config setup
+
+	// 		{
+	// 			"brand_image": {
+	// 				"full": "<path>/<to>/<external_folder>/logo-full.svg"
+	// 			}
+	// 		}
+
+
+		
+	// 	const dynamic_url = async () => {
+	// 		const getCfgPath = await fetch(document.location).then(res => {
+	// 			let xConfigPath = res.headers.get('x-config');
+	// 			return xConfigPath;
+	// 		}).catch((e) => {
+	// 			console.error(e);
+	// 			return undefined;
+	// 		})
+	// 		return getCfgPath;
+	// 	}
+	// 	// .then(() => this.App.removeSplashScreenRequestor(this));
+	// 	const dynamic_config_url = await dynamic_url();
+	// 	// Check on undefined configuration
+	// 	if (dynamic_config_url !== undefined) {
+	// 		this.App.addSplashScreenRequestor(this);
+	// 		let axios = Axios.create({ baseURL: window.location.protocol + '//' + window.location.host });
+	// 		axios.get(dynamic_config_url).then(response => {
+	// 			// Check on status and content-type
+	// 			if ((response.status === 200) && (response.headers["content-type"] !== undefined && response.headers["content-type"].includes("application/json"))) {
+	// 				this.Config._dynamic_config = response.data;
+	// 				if (this.App.Store !== undefined) {
+	// 					this.Config.dispatch(this.App.Store);
+	// 				}
+	// 			} else {
+	// 				this.App.addAlert("danger", "ASABConfigService|Incorrect/invalid config file downloaded", 5, true);
+	// 			}
+	// 		})
+	// 			.catch(error => {
+	// 				console.log(error);
+	// 				this.App.addAlert("danger", "ASABConfigService|Error when downloading a config file. The path might be corrupted", 5, true);
+	// 			})
+	// 			.then(() => this.App.removeSplashScreenRequestor(this));
+	// 	}
+	// }
 
 
 	addDefaults(defaults, override) {
