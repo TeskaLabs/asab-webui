fa45ee0202025a9bad5a83ef25c2276f4140b16f
fpesek <frantisek.pesek@teskalabs.com>
2022-09-22 18:05:28 +0200

    WIP: websocket implementation trial

--- src/modules/maintenance/MicroservicesModule/MicroservicesContainers/MicroservicesContainer.js
+++ src/modules/maintenance/MicroservicesModule/MicroservicesContainers/MicroservicesContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Container } from 'reactstrap';
@@ -16,7 +16,67 @@ export default (props) => {
 
 	const { t } = useTranslation();
 
-	const LMIORemoteControlAPI = props.app.axiosCreate('lmio_remote_control');
+
+	let wsSubPath = '/ws';
+	// const LMIORemoteControlAPI = props.app.axiosCreate('lmio_remote_control');
+	const serviceName = 'lmio_remote_control';
+	let WSUrl = props.app.getWebSocketURL(serviceName, wsSubPath);
+	console.log(WSUrl, "WS URL")
+	let WSClient = null;
+
+	const isMounted = useRef(null);
+
+
+	useEffect(() => {
+		isMounted.current = true;
+
+		if (WSUrl != undefined) {
+			console.log('RECONNECT JEDE?')
+			reconnect();
+		}
+		console.log(WSClient, "WS CLIENT")
+		return () => {
+			if (WSClient != null) {
+				try {
+					WSClient.close();
+				} catch (e) {
+					console.log("Ignored exception: ", e)
+				}
+			}
+
+			isMounted.current = false;
+		}
+	}, []);
+
+	const reconnect = () => {
+		console.log(WSClient, 'WS CLIENT V RECONNECT')
+		if (WSClient != null) {
+			try {
+				WSClient.close();
+			} catch (e) {
+				console.log("Ignored exception: ", e)
+			}
+		}
+
+		if (isMounted.current === false) return;
+
+		WSClient = props.app.createWebSocket(serviceName, wsSubPath);
+
+		console.log(WSClient, "WS CLIENT V RECONNECT 2")
+
+		WSClient.onmessage = (message) => {
+			// DO something
+			console.log(message, "MESSAGE")
+		};
+
+		WSClient.onerror = (error) => {
+			// DO something
+			console.warn(error, "ERROR Z WEBSOCKETU")
+			setTimeout(() => {
+				reconnect();
+			}, 3000, this);
+		};
+	}
 
 	const headers = [
 		{
@@ -39,39 +99,39 @@ export default (props) => {
 		{ name: t('MicroservicesContainer|Version'), key: 'version'}
 	];
 
-	// Filter the value
-	const onSearch = (value) => {
-		setFilter(value);
-	};
+	// // Filter the value
+	// const onSearch = (value) => {
+	// 	setFilter(value);
+	// };
 
-	useEffect(() => {
-		getMicroservicesList();
-	}, [page, filter ,limit]);
+	// useEffect(() => {
+	// 	getMicroservicesList();
+	// }, [page, filter ,limit]);
 
-	const getMicroservicesList = async () => {
-		try {
-			const response = await LMIORemoteControlAPI.get('/microservices', { params: { p: page, i: limit, f: filter }});
+	// const getMicroservicesList = async () => {
+	// 	try {
+	// 		const response = await LMIORemoteControlAPI.get('/microservices', { params: { p: page, i: limit, f: filter }});
 
-			if (response.data.result !== "OK") throw new Error(response);
+	// 		if (response.data.result !== "OK") throw new Error(response);
 
-			setList(response.data.data);
-			setCount(response.data.count);
-		} catch (e) {
-			console.error(e);
-			props.app.addAlert("warning", t('MicroservicesContainer|Failed to fetch data'));
-		}
-	}
+	// 		setList(response.data.data);
+	// 		setCount(response.data.count);
+	// 	} catch (e) {
+	// 		console.error(e);
+	// 		props.app.addAlert("warning", t('MicroservicesContainer|Failed to fetch data'));
+	// 	}
+	// }
 
-	const customRowStyle = {
-		style: {
-			backgroundColor: "#fff3cd",
-			color: "#856404"
-		},
-		condition: (obj) => {
-			if (obj["attention_required"] && Object.keys(obj["attention_required"]).length > 0) return true;
-			return false;
-		}
-	}
+	// const customRowStyle = {
+	// 	style: {
+	// 		backgroundColor: "#fff3cd",
+	// 		color: "#856404"
+	// 	},
+	// 	condition: (obj) => {
+	// 		if (obj["attention_required"] && Object.keys(obj["attention_required"]).length > 0) return true;
+	// 		return false;
+	// 	}
+	// }
 
 	return (
 		<Container className="svcs-container" fluid>
@@ -85,11 +145,11 @@ export default (props) => {
 				setLimit={setLimit}
 				limitValues={[20, 50, 100]}
 				search={{ icon: 'cil-magnifying-glass', placeholder: t("CredentialsListContainer|Search") }}
-				onSearch={onSearch}
+				// onSearch={onSearch}
 				title={{
 					text: t('MicroservicesContainer|Microservices'), icon: "cil-list"
 				}}
-				customRowStyle={customRowStyle}
+				// customRowStyle={customRowStyle}
 			/>
 		</Container>
 	)
