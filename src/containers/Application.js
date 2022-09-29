f10081b3392b98ddd3307d7da34132cb4cdf8e4a
akonshina <arina.konshina@teskalabs.com>
2022-09-29 14:28:10 +0200

    add class for body

--- src/containers/Application.js
+++ src/containers/Application.js
@@ -471,6 +471,7 @@ class Application extends Component {
 	}
 
 	render() {
+		// document.body.classList.add('spinner-off')
 		// Render the splash screen if needed
 		if (this.state.SplashscreenRequestors > 0) return (
 			<Provider store={this.Store}>
