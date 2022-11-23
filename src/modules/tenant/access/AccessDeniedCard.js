56c551c0397257ee09c7a56cf926d9504cc6e677
Pe5h4 <pesekf@email.cz>
2022-11-23 16:55:40 +0100

    Removal of Tenant selector card, implementation of Access denied card

---
+++ src/modules/tenant/access/AccessDeniedCard.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { locationReplace } from 'asab-webui';
+import { connect } from 'react-redux';
+
+import {
+	Card, CardHeader, CardBody, CardTitle,
+	Button, Input, Label
+} from 'reactstrap';
+
+import "./access.scss";
+
+function AccessDeniedCard(props) {
+	const { t } = useTranslation();
+	const [ accessDenied, setAccessDenied ] = useState(false);
+	const [ deniedTenant, setDeniedTenant ] = useState("");
+
+	useEffect(() => {
+		var redirectUriQs = new URLSearchParams(window.location.hash);
+		const errorType = redirectUriQs.get('error');
+		if ((errorType != undefined) && errorType.includes("access_denied")) {
+			var qs = new URLSearchParams(window.location.search);
+			const tenantParameter = qs.get('tenant');
+			setAccessDenied(true);
+			setDeniedTenant(tenantParameter);
+		}
+	}, [])
+
+	/*
+		We can't use logout/redirection to login via /openidconnect/logout,
+		since we dont have a oauth token here (access denied)
+	*/
+	const continueToLogin = async () => {
+		const SeaCatAuthAPI = props.app.axiosCreate('seacat_auth');
+		let response;
+		try {
+			// Use public logout
+			response = await SeaCatAuthAPI.put('/public/logout');
+		} catch (err) {
+			console.error(err);
+			props.app.addAlert("danger", t("AccessDeniedCard|Silly as it sounds, the logout failed"));
+		}
+		await locationReplace(`${window.location.pathname}`);
+	}
+
+	return (
+		props.app.Services.TenantService &&
+		props.app.Modules.filter(obj => obj.Name === "AuthModule").length > 0 &&
+		(accessDenied == true) ?
+			<div className="access-denied-wrapper">
+				<Card className="access-denied-card">
+					<CardHeader className="border-bottom">
+						<div className="card-header-title">
+							<CardTitle className="text-primary mb-0" tag="h2">
+								{t("AccessDeniedCard|Access denied")}
+							</CardTitle>
+						</div>
+					</CardHeader>
+					<CardBody>
+						<p>
+							{t("AccessDeniedCard|In case of difficulties, please contact your application administrator to grant you appropriate access rights for tenant")}
+						</p>
+						<p className="text-center tenant-text">
+							{deniedTenant}
+						</p>
+						<Button
+							className="justify-content-center"
+							block
+							color="primary"
+							onClick={() => {continueToLogin()}}
+						>
+							{t("AccessDeniedCard|Continue here")}
+						</Button>
+					</CardBody>
+				</Card>
+			</div>
+		: null
+	);
+}
+
+export default AccessDeniedCard;
