6dbcc28d643b9701144bb3b89bd566bc8f5242c6
akonshina <arina.konshina@teskalabs.com>
2023-01-02 09:54:24 +0100

    return preferred_username and add todo

--- src/modules/auth/header.js
+++ src/modules/auth/header.js
@@ -99,8 +99,10 @@ const mapStateToProps = state => {
 		else if (userinfo.username) {
 			username = userinfo.username;
 		}
-		else if (userinfo.username) {
-			username = userinfo.username;
+		else if (userinfo.preferred_username) {
+			// TODO: Remove preferred_username by the end of 2023
+			console.warn("preferred_username is deprecated");
+			username = userinfo.preferred_username;
 		}
 		else {
 			username = userinfo.id;	
