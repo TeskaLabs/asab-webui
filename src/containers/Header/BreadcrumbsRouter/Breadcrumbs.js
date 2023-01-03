1af8beb328f19c2c8b191c3e6ac6d0d9877ad245
akonshina <arina.konshina@teskalabs.com>
2023-01-03 14:54:28 +0100

    refactor bredcrumbs

--- src/containers/Header/BreadcrumbsRouter/Breadcrumbs.js
+++ src/containers/Header/BreadcrumbsRouter/Breadcrumbs.js
@@ -9,14 +9,21 @@ const Breadcrumbs = ({
 	routes, match, app, disableContainerBreadcrumbs
 }) => {
 	const { t } = useTranslation();
-	// Get new crumbs each time location has been changed
-	const crumbs = routes.filter(({ path }) => match.path.includes(path)).
+	const crumbs = routes.filter(({ path }) => {
+			if ((path.split("/").length - 1) === (match.path.split("/").length - 1)) { // Checking if the number of slash ('/') is the same
+				if (match.path.includes(path) && (path.match(/[^\/]+$/)[0] === match.path.match(/[^\/]+$/)[0])) { // I take the string after the last slash ('/') and check if they match each other
+					return path;
+				}
+			} else {
+				return match.path.includes(path);
+			}
+		}).
 		map(({ path, ...rest }) => ({
 			path: Object.keys(match.params).length ?
 				Object.keys(match.params).reduce((path, param) =>
 					path.replace(`:${param}`, match.params[param]), path
 				) : path,
-				...rest
+			...rest
 		})).
 		filter(crumb => crumb.name);
 
@@ -26,13 +33,14 @@ const Breadcrumbs = ({
 	// TODO: Add disabling breadcrumbs
 	return (
 		<div className="breadcrumbs p-auto">
+			{console.log(crumbs, "crumbs")}
 			<h4 className="mr-2">{t(`Breadcrumbs|${crumbs[crumbs.length-1].name}`)} {crumbs.length > 1 ? "|" : ""}</h4>
 			<Breadcrumb>
 				{crumbs.map((crumb, idx) => {
 					if (idx === (crumbs.length - 1)) return ;
 					return (
 						<BreadcrumbItem key={idx} active={idx === crumbs.length - 1}>
-							{idx !== crumbs.length - 1 ? 
+							{idx !== crumbs.length - 1 ?
 								<Link to={crumb.path}>{t(`Breadcrumbs|${crumb.name}`)}</Link>
 								: <span>{t(`Breadcrumbs|${crumb.name}`)}</span>
 							}
