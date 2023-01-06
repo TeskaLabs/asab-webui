bff653bec8ebd152a36301b7927f71fc56e3221a
Pe5h4 <pesekf@email.cz>
2023-01-06 10:00:49 +0100

    Simplified classname condtions in for toggle icon

--- src/components/TreeMenu/TreeMenuItem.js
+++ src/components/TreeMenu/TreeMenuItem.js
@@ -35,6 +35,6 @@ const TreeMenuItem = ({
 	)
 };
 
-const ToggleIcon = ({ on, selected }) => <span className="menu-span"><i className={on ? `menu-toggle-icon cil-arrow-circle-bottom ${selected}` : `menu-toggle-icon cil-arrow-circle-right ${selected}`}/></span>;
+const ToggleIcon = ({ on, selected }) => <span className="menu-span"><i className={`menu-toggle-icon ${selected} ${on ? `cil-arrow-circle-bottom` : `cil-arrow-circle-right`}`}/></span>;
 
 export default TreeMenuItem;
