251ddd8a56cf15a9d077a069492e25ba718cb328
Petr Kavulok <petrkavulok@Petrs-MBP.teskalabs.int>
2023-01-13 10:47:43 +0100

    BrandingService definition

---
+++ src/services/BrandingService.js
@@ -0,0 +1,45 @@
+import Service from '../abc/Service';
+
+export default class BrandingService extends Service {
+
+	/*
+	Branding service expects `header-logo-full-light.svg` and `header-logo-minimized-light.svg` SVG images
+	in `/public/media/logo/` directory:
+
+	* `header-logo-full-light.svg` dimensions: 120 x 30 pixels
+	* `header-logo-minimized-light.svg` dimensions: 30 x 30 pixels
+	*/
+
+	constructor(app, serviceName="BrandingService"){
+		super(app, serviceName)
+		// this.Items = [];
+	}
+
+	getLogo(config, theme) {
+		if (theme && config._dynamic_config?.brandImage) {
+			return config._dynamic_config.brandImage[theme]
+		}
+		if (theme && config._defaults)
+
+		if (theme && config._defaults?.brandImage && config._defaults?.brandImage[theme]) {
+			if (config._defaults?.brandImage[theme]?.minimized == undefined) {
+				return {
+					full: config._defaults.brandImage[theme]?.full,
+					minimized: config._defaults.defaultBrandImage.minimized,
+					href: config._defaults.brandImage?.href
+				}
+			}
+			if (config._defaults?.brandImage[theme]?.full == undefined) {
+				return {
+					full: config._defaults.defaultBrandImage.full,
+					minimized: config._defaults.brandImage[theme].minimized,
+					href: config._defaults.brandImage?.href
+				};
+			}
+			return config._defaults.brandImage[theme]
+		} else {
+			return config._defaults.defaultBrandImage;
+		}
+	}
+
+}
