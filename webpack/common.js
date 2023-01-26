c1a78bb5eae765dbcbb82fee8930229cb110e4b8
Petr Kavulok <petrkavulok@Petrs-MacBook-Pro.local>
2023-01-26 16:52:39 +0100

    css,scss,sass loaders update

--- webpack/common.js
+++ webpack/common.js
@@ -1,6 +1,5 @@
 const { execSync } = require("child_process");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require('path');
 
 ///
 var exports = module.exports = {}
@@ -40,58 +39,24 @@ exports.getRules = function(config) {
 			}],
 			exclude: '/node_modules/' // we can exclude node_modules b/c they're already complied
 		},
-
 		{
-			test: /\.(css)$/,
+			test: /\.(s[ac]|c)ss$/i,
 			use: [
-				MiniCssExtractPlugin.loader,
+				MiniCssExtractPlugin.loader, //extracs CSS into files
 				{
 					loader: 'css-loader',
-					// options: { sourceMap: true }
+					options: { sourceMap: true }
 				},
 				{
 					loader: 'postcss-loader',
-					// options: { sourceMap: true }
-				},
-			]
-		},
-		{
-			test: /\.(scss|sass)$/,
-			use: [
-				MiniCssExtractPlugin.loader,
-				{
-					loader: 'css-loader',
-					// options: { sourceMap: true }
-				},
-				{
-					loader: 'postcss-loader',
-					// options: { sourceMap: true }
+					options: { sourceMap: true }
 				},
 				{
 					loader: 'sass-loader',
-					// options: { sourceMap: true }
+					options: { sourceMap: true }
 				}
 			],
 		},
-		// {
-		// 	test: /\.(s[ac]|c)ss$/i,
-		// 	use: [
-		// 		MiniCssExtractPlugin.loader, //extracs CSS into files
-		// 		'css-loader', 'postcss-loader', 'sass-loader'
-		// 		// {
-		// 		// 	loader: 'css-loader',  // Second, turn css into commonjs
-		// 		// 	options: { sourceMap: true }
-		// 		// },
-		// 		// {
-		// 		// 	loader: 'postcss-loader',
-		// 		// 	options: { sourceMap: true }
-		// 		// },
-		// 		// {
-		// 		// 	loader: 'sass-loader', // First, turn sass into css
-		// 		// 	options: { sourceMap: true }
-		// 		// }
-		// 	],
-		// },
 		{
 			test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)(\?.*)?$/,
 			type: 'asset/resource',
