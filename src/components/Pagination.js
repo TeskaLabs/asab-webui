4dee221f89e88eff5c9430d54a51692b80eb62d0
Pe5h4 <pesekf@email.cz>
2023-02-01 10:28:28 +0100

    Update pagination to skip values in between first and last page when click on first or last page

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -16,18 +16,20 @@ export default function ({ currentPage, setPage, lastPage, style }) {
 
 	const { t, i18n } = useTranslation();
 
-	// console.log(currentPage, lastPage, "CUR LAST")
+	// Initial render with default values
 	useEffect(() => {
 		setArrayStartEnd([1, lastPage]);
 		pagesMiddle();
 	}, [slots])
 
+	// Set last page
 	useEffect(() => {
 		if (currentPage > lastPage && lastPage != 0) {
 			setPage(lastPage);
 		}
 	}, [lastPage])
 
+	// Insert '...' with more or less variables to an middle part array
 	const pages = useMemo(() => {
 		if (middlePart.length > 0) {
 			let p = [...arrayStartEnd];
@@ -50,12 +52,17 @@ export default function ({ currentPage, setPage, lastPage, style }) {
 	}, [middlePart])
 
 
+	// Compute pages to display in the middle of first and last page
 	const pagesMiddle = (action) => {
 		let p = []; // Pages array
 		let start = currentPage - Math.floor(slots / 2);
 		let end = currentPage + Math.floor(slots / 2);
 
 		if ((action == "less") || (action == "more")) {
+			/*
+				Compute start and end pages (increment or decrement) of a page
+				'...' value (more or less) click
+			*/
 			if (action == "less") {
 				start = middlePart[0] - 1;
 				end = middlePart[middlePart.length - 1] - 1;
@@ -80,6 +87,7 @@ export default function ({ currentPage, setPage, lastPage, style }) {
 			}
 			setMiddlePart(p);
 		} else {
+			// Set start and end variables
 			if (start < 1) {
 				start = 1;
 				end = Math.min(slots, lastPage);
@@ -87,43 +95,44 @@ export default function ({ currentPage, setPage, lastPage, style }) {
 				start = Math.max(lastPage - slots + 1, 1);
 				end = lastPage;
 			}
-			// console.log(start, end, 'TAK CO TADY')
-			for (let i = start + 1; i < end; i++) {
-				p.splice(i - 1 , 0, i)
-				if ((i == 5) && (end > 5)) {
-					setMiddlePart(p);
-					return;
-				}
-			}
-			// TODO: Manage cases when user clicks on last and first item and would like to skip all the other pages
-			// console.log(action, 'ACTION')
-			// if (action == lastPage) {
-			// 	let e = end - 4;
-			// 	if (e < 1) {
-			// 		e = start + 1;
-			// 	}
-			// 	console.log(e, "AAAAAAAAAAAAAA")
-			// 	for (let i = e; i < end; i++) {
-			// 		p.splice(i - 1 , 0, i)
-			// 		if ((i == 5) && (end > 5)) {
-			// 			setMiddlePart(p);
-			// 			return;
-			// 		}
-			// 	}
-			// } else {
-			// 	console.log("TADU", start, end)
-			// 	for (let i = start + 1; i < end; i++) {
-			// 		p.splice(i - 1 , 0, i)
-			// 		if ((i == 5) && (end > 5)) {
-			// 			setMiddlePart(p);
-			// 			return;
-			// 		}
+			// // console.log(start, end, 'TAK CO TADY')
+			// for (let i = start + 1; i < end; i++) {
+			// 	p.splice(i - 1 , 0, i)
+			// 	if ((i == 5) && (end > 5)) {
+			// 		setMiddlePart(p);
+			// 		return;
 			// 	}
 			// }
+			// TODO: Manage cases when user clicks on last and first item and would like to skip all the other pages
+
+			if (action == lastPage) {
+				/*
+					Triggers only when clicking on last page
+					and computes the appropriate page values
+				*/
+				let e = end - 4;
+				if (e <= 1) {
+					e = start + 1;
+				}
+				for (let i = e; i < end; i++) {
+					p.splice(i - 1 , 0, i)
+				}
+				setMiddlePart(p);
+			} else {
+				// Triggers initially and on 1st page click
+				for (let i = start + 1; i < end; i++) {
+					p.splice(i - 1 , 0, i)
+					if ((i == 5) && (end > 5)) {
+						setMiddlePart(p);
+						return;
+					}
+				}
+			}
 		}
 		setMiddlePart(p);
 	}
 
+	// Action triggered on value click in page dropdown
 	const pageDropdownClick = (p) => {
 		if (p == "more") {
 			pagesMiddle(p);
@@ -132,10 +141,13 @@ export default function ({ currentPage, setPage, lastPage, style }) {
 			pagesMiddle(p);
 			setOpen(false);
 		} else {
-			// TODO: Manage cases when user clicks on last and first item and would like to skip all the other pages
-			// if ((p == lastPage) || (p == 1)) {
-			// 	pagesMiddle(p);
-			// }
+			/*
+				Manage cases when user clicks on last or first item
+				and would like to skip all the other pages in between
+			*/
+			if ((p == lastPage) || (p == 1)) {
+				pagesMiddle(p);
+			}
 			setPage(p);
 			setOpen(true);
 		}
