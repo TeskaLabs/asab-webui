2f4b38da379c070c1e5480e63afbc91210ce94a4
Petr Kavulok <petrkavulok@Petrs-MacBook-Pro.local>
2023-02-21 09:59:57 +0800

    obsolete temporary fix removal

--- src/actions.js
+++ src/actions.js
@@ -11,4 +11,3 @@ export const SET_DEV_CONFIG = "asab/setDevConfig";
 
 export const SET_HIDDEN_ITEMS_SIDEBAR = "asab/setHiddenItemsSidebar";
 export const COLLAPSE_SIDEBAR = "asab/collapseSidebar";
-export const SET_SMALL_SIDEBAR = "asab/setSmallSidebar";
