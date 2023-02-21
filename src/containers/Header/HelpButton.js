83333cba0e0ac2047630f33a5b31ff01ed553fe0
akonshina <arina.konshina@teskalabs.com>
2023-02-21 15:49:28 +0100

    add help component

--- src/containers/Header/HelpButton.js
+++ src/containers/Header/HelpButton.js
@@ -1,19 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { connect } from 'react-redux';
+import {useTranslation} from "react-i18next";
+import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
 
-import { NavLink } from 'reactstrap';
+import './header.scss';
 
-const HelpButton = ({ url, icon = "cil-info", target="_blank" }) => {
-    if (!url) return null;
+import {Modal, NavLink, Card, CardHeader, CardBody, Button} from 'reactstrap';
+
+
+const HelpButton = () => {
+    // if (!url) return null;
+    const { t, i18n } = useTranslation();
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
 
     return (
-        <NavLink 
-            style={{ fontSize: "1.2rem", marginRight: "1rem" }}
-            href={url}
-            target={target}
-        >
-            <i className={icon}></i>
-        </NavLink>
+        <>
+            <NavLink
+                className="help-button"
+                onClick={toggle}
+                title={t("Show help info")}
+                href="#"
+            >
+                <i className="cil-info"></i>
+            </NavLink>
+
+            <Modal isOpen={modal} toggle={toggle} className="center help-modal-card">
+                <Card>
+                    <CardHeader className="border-bottom">
+                        <div className="card-header-title">
+                            <i className="cil-info pr-2" />
+                            Help Card
+                        </div>
+                        <Button color="danger" onClick={toggle}>
+                            <i className="cil-info"></i>
+                        </Button>
+                    </CardHeader>
+                    <CardBody>
+                        <ReactMarkdown
+                            rehypePlugins={[rehypeRaw]}
+                            width="100%"
+                            height="100%"
+                        >
+                            {
+                                "empty\nbooo\nmbmbmbmbhiii\n\n## Hello title\n"}
+                            {/*{description}*/}
+                        </ReactMarkdown>
+                    </CardBody>
+                </Card>
+            </Modal>
+        </>
     );
 }
 
