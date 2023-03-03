a0eb6a3ae7d967a3354380d57fa8f86351dc5048
petrKavulok <79644454+petrKavulok@users.noreply.github.com>
2023-03-03 21:55:26 +0800

    Refactor/webpack-update (#379)
    * webpack update
    
    * added missing action
    
    * message object adjustment
    
    * config update
    
    * obsolete console log removal
    
    * css, scss, sass rules adjustment
    
    * MiniCssExtractPlugin application, HtmlWebpackPlugin minification
    
    * v3 to v4 version migration
    
    * js output filename update, comments
    
    * output files naming update
    
    * css and sass loaders split
    
    * syntax update
    
    * update of deprecated code
    
    * remove obsolete console log
    
    * css,scss,sass loaders update
    
    * minifier, TerserPlugin, optimization
    
    * obsolete temporary fix removal
    
    * update route for filename in MiniCssExtractPlugin
    
    * remove watch: true from dev config, causing deprecation warning
    
    * deprecation warnings removal adjustments
    
    * babel loader finetuning
    
    * filename route adjustment for MiniCssExtractPlugin, obsolete code removal
    
    * loader for fonts init
    
    * filename update in MiniCssExtractPlugin
    
    * removing comments extraction which generated licence.txt files
    
    * indentation fix
    
    * CHANGELOG.md update
    
    * DEMO package.json update
    
    * package.json update, 'react-dom': '^17.0.0', 'css-minimizer-webpack-plugin': '^4.2.2'
    
    ---------
    
    Co-authored-by: Petr Kavulok <petrkavulok@Petrs-MBP.teskalabs.int>
    Co-authored-by: Petr Kavulok <petrkavulok@Petrs-MacBook-Pro.local>

--- script/start.js
+++ script/start.js
@@ -6,7 +6,7 @@ const WebpackDevServer = require('webpack-dev-server');
 
 // Parse arguments
 var parser = common.getArgumentParser();
-var args = parser.parseArgs();
+var args = parser.parse_args();
 
 // Load config
 var config = common.loadConfig(args)
@@ -15,8 +15,11 @@ var config = common.loadConfig(args)
 // Compiler
 const webpackConf = require(path.resolve(__dirname, '..', 'webpack', 'webpack.dev.js')).build(config);
 const webpackDevServerConf = require(path.resolve(__dirname, '..', 'webpack', 'webpackDevServer.js')).build(config);
-const compiler = webpack(webpackConf)
+const compiler = webpack(webpackConf, undefined);
 
 // Dev server
-const devServer = new WebpackDevServer(compiler, webpackDevServerConf);
-devServer.listen(config.webpackDevServer.port)
+const devServer = new WebpackDevServer(webpackDevServerConf, compiler);
+(async () => {
+    await devServer.start();
+    console.log(`Dev server is listening on port ${config.webpackDevServer.port}`);
+  })()
