9d0b465814fcf0eabb2d22c9f124d71070833994
Pe5h4 <pesekf@email.cz>
2023-03-16 12:23:08 +0100

    Update Datetime component to return simple string time

--- doc/DateTime.md
+++ doc/DateTime.md
@@ -10,7 +10,6 @@ import { DateTime } from 'asab-webui';
 ...
 
 <DateTime value="2020-08-22T00:00:00+00:00" dateTimeFormat="long"/>
-
 ```
 
 The input `value` is in UTC and one of:
@@ -35,24 +34,24 @@ The prop `dateTimeFormat` specify length of the time.
 > - `dateTimeFormat="iso"` is to add milliseconds to the time -> `2022-11-03T13:13:00+01:00`. 
 
 
-# ASAB WebUI timeToString method
+# ASAB WebUI DateTime as a string
 
-`timeToString` is a method returning date and time as a simple string in a format based on passed `lang` parameter. 
-Acccepts 2 paramaters
-    - timestamp - in seconds or miliseconds
-    - language object (coming from `useDateFNSLocale()`) of selected locale - default `undefined` shows `en` format (e.g. `'Apr 11, 2022, 1:16 PM'`)
+`DateTime as a string` is a method returning date and time as a simple string.
+
+Acccepts 3 paramaters
+    - value - `time` value
+    - dateTimeFormat - format of the returned string, by default it is `medium`
+    - renderType - `"string"` - this prop defines that the DateTime should be returned not as a component, but as a string
 
 ## Example code
 
 ```javascript
-import { timeToString, useDateFNSLocale } from 'asab-webui';
+import { DateTime } from 'asab-webui';
 ...
 
-const myFunction = (time) => {
-    const lang = useDateFNSLocale();
-    let timestring = timeToString(time, lang);
-    
-    return timestring;
+const myFunction = (value) => {
+    return DateTime({value: value, dateTimeFormat: "long", renderType: "string"});
 }
 
+...
 ```
--- src/components/DateTime/index.js
+++ src/components/DateTime/index.js
@@ -8,18 +8,33 @@ export function DateTime(props) {
 	const locale = useDateFNSLocale();
 
 	if ((props.value === null) || (props.value === undefined)) {
-		return (
-			<span className="datetime">{' '}</span>
-		);
+		switch(props.renderType) {
+			case "component": return (
+				<span className="datetime">{' '}</span>
+			);
+			case "string": return " ";
+			default: return (
+				<span className="datetime">{' '}</span>
+			);
+		}
 	}
 
 	if (new Date(props.value).toString() === "Invalid Date") {
-		return (
-			<span className='datetime'>
-				<i className="cil-clock pr-1"></i>
-				Invalid Date
-			</span>
-		);
+		switch(props.renderType) {
+			case "component": return (
+				<span className='datetime'>
+					<i className="cil-clock pr-1"></i>
+					Invalid Date
+				</span>
+			);
+			case "string": return "Invalid Date";
+			default: return (
+				<span className='datetime'>
+					<i className="cil-clock pr-1"></i>
+					Invalid Date
+				</span>
+			);
+		}
 	}
 
 	const date = timeToString(props.value, props.dateTimeFormat, locale);
@@ -28,10 +43,19 @@ export function DateTime(props) {
 		props.value > 9999999999 ? formatDistanceToNow(props.value, { locale: locale }) :
 		formatDistanceToNow(props.value * 1000, { locale: locale });
 
-	return (
-		<span className="datetime" title={dateFromNow}>
-			<i className="cil-clock pr-1"></i>
-			{date}
-		</span>
-	)
+	switch(props.renderType) {
+		case "component": return (
+			<span className="datetime" title={dateFromNow}>
+				<i className="cil-clock pr-1"></i>
+				{date}
+			</span>
+		);
+		case "string": return date;
+		default: return (
+			<span className="datetime" title={dateFromNow}>
+				<i className="cil-clock pr-1"></i>
+				{date}
+			</span>
+		);
+	}
 }
