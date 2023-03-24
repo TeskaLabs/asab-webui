9fc56d65fc92436493a544935057ce26807cd05a
akonshina <arina.konshina@teskalabs.com>
2023-03-24 11:46:43 +0100

    reset helpconten

--- src/containers/Application.js
+++ src/containers/Application.js
@@ -442,7 +442,12 @@ class Application extends Component {
 
 	addHelpButton(path) {
 		useEffect(() => {
-			this.HelpService.setData(path);
+			this.HelpService.setData(path);return () => {
+				this.Store.dispatch({
+					type: HELP_CONTENT,
+					content: ""
+				})
+			}
 		}, [])
 	}
 
