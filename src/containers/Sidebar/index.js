733f871c7e7ac9c7c9a3adca97ee12f9ddab0029
akonshina <arina.konshina@teskalabs.com>
2023-03-30 08:25:41 +0200

    remove unused import

--- src/containers/Sidebar/index.js
+++ src/containers/Sidebar/index.js
@@ -1,5 +1,5 @@
 import React, {useMemo, useEffect, useState} from 'react';
-import {connect, useDispatch, useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 
 import {Modal, Nav} from 'reactstrap';
 import SidebarItem from './SidebarItem';
