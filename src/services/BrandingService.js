cdaf73fcebede2daafb2fa2c4cee2a5198ce9600
petrKavulok <petr.kavulok@gmail.com>
2023-03-30 08:07:29 +0200

    BrandingService getLogo conditions update

--- src/services/BrandingService.js
+++ src/services/BrandingService.js
@@ -32,80 +32,48 @@ export default class BrandingService extends Service {
 		}
 	}
 
-	getLogo(theme, type="brandImage") {
-
-		// let dynamicConfig = config._dynamic_config;
-		// console.log('dynamicConfig:', dynamicConfig)
-
-		if ((type === 'sidebarLogo') && theme && this.sidebarLogo && this.sidebarLogo[theme]) {
-			return this.sidebarLogo[theme];
-		} else if ((type === 'sidebarLogo') && theme && this.sidebarLogo && !this.sidebarLogo[theme]) {
-			return defaultBrandImage
-		}
-
-		// when we have defined both cases (full & minimized) for light mode, but no dark mode -> light versions for both cases
-		// if ((dynamicConfig?.brandImage?.light?.full) && (dynamicConfig?.brandImage?.light?.minimized) && (dynamicConfig?.brandImage?.dark == undefined)) {
-		// 	console.log('we here ln 33')
-		// 	dynamicConfig.brandImage.dark = dynamicConfig.brandImage.light;
-		// }
-		// // when we have defined both cases (full & minimized) for dark mode, but no light mode -> light versions for both cases
-		// if ((dynamicConfig?.brandImage?.dark?.full) && (dynamicConfig?.brandImage?.dark?.minimized) && (dynamicConfig?.brandImage?.light == undefined)) {
-		// 	console.log('we here ln 38')
-		// 	dynamicConfig.brandImage.light = dynamicConfig.brandImage.dark;
-		// }
-
-		// if ((!dynamicConfig?.brandImage?.light) && (dynamicConfig?.brandImage?.dark?.full == undefined) && (dynamicConfig?.brandImage?.dark?.minimized)) {
-		// 	console.log('we here ln 43')
-		// 	dynamicConfig.brandImage.light = dynamicConfig.brandImage.dark;
-		// }
-		// if ((!dynamicConfig?.brandImage?.light) && (dynamicConfig?.brandImage?.dark?.minimized == undefined) && (dynamicConfig?.brandImage?.dark?.full)) {
-		// 	console.log('we here ln 47')
-		// 	dynamicConfig.brandImage.light = dynamicConfig.brandImage.dark;
-		// }
-		// if ((!dynamicConfig?.brandImage?.dark) && (dynamicConfig?.brandImage?.light?.full == undefined) && (dynamicConfig?.brandImage?.light?.minimized)) {
-		// 	console.log('we here ln 51')
-		// 	dynamicConfig.brandImage.dark = dynamicConfig.brandImage.light;
-		// }
-		// if ((!dynamicConfig?.brandImage?.dark) && (dynamicConfig?.brandImage?.light?.minimized == undefined) && (dynamicConfig?.brandImage?.light?.full)) {
-		// 	console.log('we here ln 55')
-		// 	dynamicConfig.brandImage.dark = dynamicConfig.brandImage.light;
-		// }
-
-		// if (theme && dynamicConfig?.brandImage) {
-		// 	if (dynamicConfig.brandImage[theme].full == undefined) {
-		// 		return {
-		// 			full: dynamicConfig.brandImage[theme].minimized,
-		// 			minimized: dynamicConfig.brandImage[theme].minimized
-		// 		}
-		// 	}
-		// 	if (dynamicConfig.brandImage[theme].minimized == undefined) {
-		// 		return {
-		// 			full: dynamicConfig.brandImage[theme].full,
-		// 			minimized: dynamicConfig.brandImage[theme].full
-		// 		}
-		// 	}
-		// 	return dynamicConfig.brandImage[theme]
-		// }
-
-		if (theme && this.brandImage && this.brandImage[theme]) {
-			if (this.brandImage[theme]?.minimized == undefined) {
-				return {
-					full: this.brandImage[theme]?.full,
-					minimized: this.defaultBrandImage.minimized,
-					href: this.brandImage?.href
-				}
-			}
-			if (this.brandImage[theme]?.full == undefined) {
-				return {
+	determineSources(imgObject, theme) {
+		if (imgObject[theme]?.minimized && imgObject[theme]?.full) {
+			return {
+				...imgObject[theme],
+				href: imgObject.href ?? '/'
+			};
+		} else if (imgObject[theme]?.minimized) {
+			return {
 					full: this.defaultBrandImage.full,
-					minimized: this.brandImage[theme].minimized,
-					href: this.brandImage?.href
+					minimized: imgObject[theme].minimized,
+					href: imgObject?.href ?? '/'
 				};
+		} else if (imgObject[theme]?.full) {
+			return {
+				full: imgObject[theme].full,
+				minimized: this.defaultBrandImage.minimized,
+				href: imgObject?.href ?? '/'
 			}
-			return this.brandImage[theme]
+		}
+	}
+
+	getLogo(theme, type="brandImage") {
+		if((type === 'sidebarLogo') && theme && this.sidebarLogo) {
+			if (this.sidebarLogo[theme]) {
+				return this.sidebarLogo[theme];
+			} else if (!this.sidebarLogo[theme]) {
+				return this.sidebarLogo[theme === 'dark' ? 'light' : 'dark'];
+			}
+		}
+		console.log( 'theme', theme);
+		console.log( 'this.brandImage', this.brandImage);
+		console.log( 'this.brandImage[theme]', this.brandImage[theme]);
+		if (theme && this.brandImage) {
+			if (this.brandImage[theme] !== undefined) {
+				return this.determineSources(this.brandImage, theme)
+			}
+			//  else {
+			// 	console.log('tadyyyyy')
+			// 	return this.determineSources(this.brandImage, theme);
+			// }
 		} else {
 			return this.defaultBrandImage;
 		}
 	}
-
 }
