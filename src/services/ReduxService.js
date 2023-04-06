cdcb810cbe9d322566733afb4eb9b05cbe6b0fe1
Pe5h4 <pesekf@email.cz>
2023-04-06 15:54:10 +0200

    Update indentation

--- src/services/ReduxService.js
+++ src/services/ReduxService.js
@@ -1,9 +1,9 @@
- export default class ReduxService {
-    constructor(){
-        this.Reducers = {}
-    }
+export default class ReduxService {
+	constructor(){
+		this.Reducers = {}
+	}
 
-    addReducer(name, reducer) {
+	addReducer(name, reducer) {
 		if (name in this.Reducers) {
 			console.warn(`Reducer with name ${name} already exists.`);
 			return;
