f8af15820a780897ea05f09c8643296384d1d52f
petrKavulok <petr.kavulok@gmail.com>
2023-05-03 16:51:44 +0200

    brandImage component init

--- src/index.js
+++ src/index.js
@@ -20,5 +20,6 @@ export { default as useDateFNSLocale } from './components/DateTime/useDateFNSLoc
 export { validateConfiguration } from './config/validateConfiguration';
 export { default as TreeMenu } from './components/TreeMenu';
 export { locationReplace } from './components/locationReplace';
+export { getBrandImage } from './components/BrandImage'
 
 import "./styles/index.scss";
