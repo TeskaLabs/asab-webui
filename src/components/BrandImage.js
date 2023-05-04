3dce26666adcbee988e0d2e890b17a41c1076092
petrKavulok <petr.kavulok@gmail.com>
2023-05-04 15:23:17 +0200

    brandimage function - refactored theme as a prop

--- src/components/BrandImage.js
+++ src/components/BrandImage.js
@@ -1,16 +1,11 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+export function getBrandImage(props, theme='light', type="brandImage") {
 
-export function getBrandImage (props) {
-    const theme = useSelector(state => state?.theme);
 	const BrandingService = props.app.Services.BrandingService;
 	let brandImage;
 
-	useEffect(() => {
-		if (BrandingService && theme) {
-			brandImage = BrandingService.getLogo(theme, 'sidebarLogo');
-		}
-	}, [theme]);
+	if (BrandingService && theme) {
+		brandImage = BrandingService.getLogo(theme, type);
+	}
 
-    return brandImage;
+	return brandImage;
 };
