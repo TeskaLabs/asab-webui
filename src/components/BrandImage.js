635fc713b34b105765332d9dfd800ee5f1bde353
petrKavulok <petr.kavulok@gmail.com>
2023-05-05 12:00:00 +0200

    getBrandImage usage example

--- src/components/BrandImage.js
+++ src/components/BrandImage.js
@@ -1,3 +1,40 @@
+/*
+	getBrandImage function returns a brandImage object based on theme.
+
+	usage:
+	```javascript
+		import { useState, useEffect } from 'react';
+		import { getBrandImage } from 'asab-webui';
+		import { useSelector } from 'react-redux';
+
+		const YourCode = (props) => {
+			const [ brandImage, setBrandImage ] = useState({});
+			const theme = useSelector(state => state.theme);
+
+			useEffect(() => {
+				setBrandImage(getBrandImage(props, theme));
+			}, [theme]);
+
+			...
+
+			return (
+				<>
+
+				...
+
+					<img src={brandImage?.full}/>
+
+				...
+
+				</>
+			)
+		};
+
+		export default YourCode
+
+	```
+*/
+
 export function getBrandImage(props, theme='light', type="brandImage") {
 
 	const BrandingService = props.app.Services.BrandingService;
