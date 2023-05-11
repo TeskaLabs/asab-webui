bcec867ae3b47146e4533a8dbf136ce75a1a923f
akonshina <arina.konshina@teskalabs.com>
2023-05-11 11:14:56 +0200

    add helpcache

--- src/services/HelpService.js
+++ src/services/HelpService.js
@@ -5,25 +5,35 @@ export default class HelpService extends Service {
 	constructor(app, serviceName="HeaderService"){
 		super(app, serviceName);
 		this.App = app;
+		this.HelpCache = {};
 	}
 
 	async setData(path) {
-		let pathWithExtension;
 
-		if ((/\.[^/.]+$/.test(path))) {
-			pathWithExtension = path;
-		} else {
-			pathWithExtension = `${path}.json`
+		if (this.HelpCache[path] != undefined) {
+			if (this.HelpCache[path] != "") {
+				this.App.Store.dispatch({
+					type: HELP_CONTENT,
+					content: this.HelpCache[path]
+				});
+				return;
+			}
+			this.App.Store.dispatch({
+				type: HELP_CONTENT,
+				content: ""
+			});
+			return;
 		}
 
 		try {
 			const ASABLibraryAPI = this.App.axiosCreate('asab_library');
-			let response = await ASABLibraryAPI.get(`/library/item/Help/${pathWithExtension}`);
+			let response = await ASABLibraryAPI.get(`/library/item/Help/${path}`);
 			if ((response.status == 200) && response.data) {
 				this.App.Store.dispatch({
 					type: HELP_CONTENT,
 					content: response.data.content
 				});
+				this.HelpCache[path] = response.data.content;
 			}
 		} catch (e) {
 			console.warn(`Help service can't retrieve data for ${path}`, e);
@@ -32,6 +42,7 @@ export default class HelpService extends Service {
 				type: HELP_CONTENT,
 				content: ""
 			});
+			this.HelpCache[path] = "";
 		}
 	}
 }
