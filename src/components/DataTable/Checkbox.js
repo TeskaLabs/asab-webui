1d67cfac4fa0f9ff8afe764881c584eae609eacd
petrKavulok <petr.kavulok@gmail.com>
2023-07-03 11:55:45 +0200

    component styling update

--- src/components/DataTable/Checkbox.js
+++ src/components/DataTable/Checkbox.js
@@ -2,10 +2,19 @@ import { Input } from "reactstrap";
 import  React, { useEffect } from "react";
 
 const Checkbox = (props) => {
-    console.log('renderd checkbox')
-    return (
-        <Input type="checkbox"></Input>
-    )
+	console.log('renderd checkbox')
+	return (
+		<div
+			style={{display: 'flex', alignItems: 'center' }}
+		>
+			{props?.checkbox?.title}
+			<Input
+				style={{marginLeft: 'unset', marginTop: 'unset', position: 'inherit'}}
+				className="ml-1"
+				onChange={props.onCheckbox}
+				type="checkbox"></Input>
+		</div>
+	)
 }
 
 export default Checkbox;
