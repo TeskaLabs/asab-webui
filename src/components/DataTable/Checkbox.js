c003c3b4c7bc130fb3f64743c794d55fcf983751
petrKavulok <petr.kavulok@gmail.com>
2023-07-03 09:17:52 +0200

    Checkbox component in DataTable initiation

---
+++ src/components/DataTable/Checkbox.js
@@ -0,0 +1,11 @@
+import { Input } from "reactstrap";
+import  React, { useEffect } from "react";
+
+const Checkbox = (props) => {
+    console.log('renderd checkbox')
+    return (
+        <Input type="checkbox"></Input>
+    )
+}
+
+export default Checkbox;
