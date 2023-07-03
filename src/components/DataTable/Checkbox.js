b14d408e6762400206b74ab137e0100ee13ba4bf
petrKavulok <petr.kavulok@gmail.com>
2023-07-03 16:35:09 +0200

    checkbox layout updatE

--- src/components/DataTable/Checkbox.js
+++ src/components/DataTable/Checkbox.js
@@ -2,17 +2,20 @@ import { Input } from "reactstrap";
 import  React, { useEffect } from "react";
 
 const Checkbox = (props) => {
-	console.log('renderd checkbox')
+	// console.log('renderd checkbox')
 	return (
 		<div
 			style={{display: 'flex', alignItems: 'center' }}
 		>
-			{props?.checkbox?.title}
 			<Input
 				style={{marginLeft: 'unset', marginTop: 'unset', position: 'inherit'}}
 				className="ml-1"
 				onChange={props.onCheckbox}
-				type="checkbox"></Input>
+				type="checkbox"
+                checked={props?.checkbox?.selectAll}
+            />
+
+			<span style={{marginLeft: '0.5rem'}}>{props?.checkbox?.title}</span>
 		</div>
 	)
 }
