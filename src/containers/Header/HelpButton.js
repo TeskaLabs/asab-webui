3970a32167c3c90f49aa5f28f1433f4341570fa1
Zenker <katja.zenker@teskalabs.com>
2023-07-03 09:45:47 +0200

    Revert "Delete HelpButton.js"
    This reverts commit c289a831ad383d8f87c4edde5a62ef8e8f3d4732.

---
+++ src/containers/Header/HelpButton.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { useTranslation } from "react-i18next";
+import { useSelector } from 'react-redux';
+
+import { Modal, NavLink, Card, CardHeader, CardBody, Button } from 'reactstrap';
+
+
+export default function HelpButton() {
+    const { t } = useTranslation();
+
+    const [modal, setModal] = useState(false);
+
+    const content = useSelector(state => state?.header.content);
+    if ((content == undefined) || (content == "")) return null;
+
+    const toggle = () => setModal(!modal);
+
+    return (
+        <>
+            <NavLink
+                className="help-button"
+                onClick={toggle}
+                title={t("Show help info")}
+                href="#"
+            >
+                <i>?</i>
+            </NavLink>
+
+            <Modal isOpen={modal} toggle={toggle} className="center help-modal-card">
+                <Card>
+                    <CardHeader className="border-bottom">
+                        <div className="card-header-title">
+                            <i className="cil-info pr-2" />
+                            {t("HelpButton|Help")}
+                        </div>
+                        <Button outline color="primary" onClick={toggle}>
+                            <i className="cil-x"/>
+                        </Button>
+                    </CardHeader>
+                    <CardBody>
+                        <div>{content}</div>
+                        {/*TODO: uncomment and use when the documentation is ready to be displayed in the iframe*/}
+                        {/*<iframe className="help-iframe" src="" title=""/>*/}
+                    </CardBody>
+                </Card>
+            </Modal>
+        </>
+    );
+}
+
