852d29a6bd456ea80ca6f365bb78aea24987bf35
queuing4oranges <katjazenker@gmail.com>
2023-07-24 16:15:47 +0200

    updated preview-flag for implementation

--- src/containers/Header/PreviewFlag.js
+++ src/containers/Header/PreviewFlag.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {useTranslation} from "react-i18next";
 
 export default function PreviewFlag({ name }) {
-    if(!name) {
-        return null
+    const flagName = name?.toLowerCase();
+    const {t} = useTranslation();
+
+    if(flagName === "preview") {
+        name = t("AnalysisContainer|Preview")
+
+    } else if(flagName === "alpha") {
+        name = "Alpha";
+
+    } else if(flagName === "beta") {
+        name = "Beta";
+    } else {
+        return null;
     }
 
     return (
