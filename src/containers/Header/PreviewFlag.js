633b5632b6b2789e08b7c819369eb1f5fcae3d77
queuing4oranges <katjazenker@gmail.com>
2023-07-25 14:53:51 +0200

    styling changes in preview flag

--- src/containers/Header/PreviewFlag.js
+++ src/containers/Header/PreviewFlag.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import {useTranslation} from "react-i18next";
+import React, { useEffect } from 'react';
+import { useTranslation } from "react-i18next";
+import { useSelector } from 'react-redux';
 
 export default function PreviewFlag({ name }) {
     const flagName = name?.toLowerCase();
-    const {t} = useTranslation();
+    const { t } = useTranslation();
+    const theme = useSelector(state=>state?.theme);
 
+    useEffect(() => {
+        const element = document.querySelector('.preview-flag');
+        if (element) {
+          element.classList.toggle('preview-flag-dark', theme === 'dark');
+          element.classList.toggle('preview-flag-light', theme === 'light');
+        }
+    }, [theme]);
+ 
     if(flagName === "preview") {
         name = t("AnalysisContainer|Preview")
-
     } else if(flagName === "alpha") {
         name = "Alpha";
-
     } else if(flagName === "beta") {
         name = "Beta";
     } else {
@@ -19,7 +27,7 @@ export default function PreviewFlag({ name }) {
 
     return (
         <>
-            <span className="preview-flag">
+            <span className="badge badge-warning mr-4 preview-flag">
                 { name }
             </span>
         </>
